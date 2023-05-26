0595a1ff7fc6bb17e4c8accb7aa48877379132e2
Lluís Tallada <lluis@thecolvinco.com>
2023-05-26 16:37:08 +0200

    fix ts errors on build process

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,7 +2,7 @@ import resolveConfig from 'tailwindcss/resolveConfig';
 
 import tailwindConfig from '../../tailwind.config';
 
-const fullTailwindConfig = resolveConfig(tailwindConfig);
+const fullTailwindConfig: any = resolveConfig(tailwindConfig);
 const hgColors = fullTailwindConfig.theme.colors.hg;
 
 export const HOLAGLOW_COLORS: any = {
