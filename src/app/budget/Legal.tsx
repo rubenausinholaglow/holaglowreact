ac9eb48942bdc926aa8dab4fdbcd0532b71bad30
Lluís Tallada <lluis@thecolvinco.com>
2023-05-30 10:13:52 +0200

    add legal text in budget

---
+++ src/app/budget/Legal.tsx
@@ -0,0 +1,14 @@
+export default function Legal() {
+  return (
+    <section className='p-8 text-[#CBD2E0] mb-8'>
+      <h3 className='text-md font-semibold mb-4'>Garantía y Política de reembolso</h3>
+      <p className='text-sm leading-6'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum
+      </p>
+    </section>
+  );
+}
--- src/app/budget/page.tsx
+++ src/app/budget/page.tsx
@@ -3,6 +3,7 @@ import Products from './Products';
 import AlmaPayment from './AlmaPayment';
 import Simulation from './Simulation';
 import PromoCode from './PromoCode';
+import Legal from './Legal';
 import Footer from './Footer';
 
 const fetchBudgetData = async (id: number) => {
@@ -54,6 +55,7 @@ export default async function Budget({
           {/* @ts-ignore */}
           {simulations.length > 0 && <Simulation simulations={simulations} />}
           <PromoCode discountCode={discountCode} discountAmount={discountAmount} />
+          <Legal />
           <Footer clinicInfo={clinicInfo} />
         </div>
       ) : (
