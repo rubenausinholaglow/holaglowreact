7d68bdd44fc532a2f32b44bc93608f8ac8c4da04
Lluís Tallada <lluis@thecolvinco.com>
2023-06-29 15:58:04 +0200

    adding temp examples in Button

--- src/components/Buttons/Buttons.stories.tsx
+++ src/components/Buttons/Buttons.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Flex } from 'components/Layouts';
-
 import { Button } from './Buttons';
+import { Flex } from 'components/Layouts/Layouts';
+import { SvgArrowSmallLeft } from 'icons/Icons';
 
 const meta: Meta<typeof Button> = {
   component: Button,
@@ -10,15 +10,33 @@ const meta: Meta<typeof Button> = {
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
+export const ButtonDefault: Story = {
   args: {
     type: 'primary',
     children: 'Button',
   },
 };
 
+const buttonWithIconContent = () => {
+  return (
+    <Flex layout='row-left'>
+      <SvgArrowSmallLeft height={20} width={20} />
+      <span className='ml-2'>Atrás</span>
+    </Flex>
+  );
+};
+
+export const ButtonWithIcon: Story = {
+  args: {
+    type: 'primary',
+    children: buttonWithIconContent(),
+  },
+};
+
+/* 
 export const Secondary: Story = {
   args: {
     type: 'secondary',
@@ -32,3 +50,12 @@ export const Tertiary: Story = {
     children: 'Button',
   },
 };
+
+export const LargeButton: Story = {
+  args: {
+    type: 'primary',
+    size: 'lg',
+    children: 'Button',
+  },
+};
+ */
--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react';
 
 export const Button = ({
   type = 'primary',
+  size = 'md',
   route = '',
   className = '',
   onClick = undefined,
@@ -11,6 +12,7 @@ export const Button = ({
   ...rest
 }: {
   type: 'primary' | 'secondary' | 'tertiary';
+  size: 'sm' | 'md' | 'lg';
   route?: string;
   className?: string;
   onClick?: () => void;
@@ -21,9 +23,12 @@ export const Button = ({
     primary: 'bg-[#7516E9] text-white rounded-full px-4 py-2',
     secondary: 'bg-[#be9ee5] text-white rounded-full px-4 py-2',
     tertiary: 'text-[#7516E9] border border-[#E2E7F0] rounded-full px-4 py-2',
+    sm: 'text-xs',
+    md: 'text-base',
+    lg: 'text-xl px-6',
   };
 
-  const styles = twMerge(`${StylesConfig[type]} ${className}`);
+  const styles = twMerge(`${StylesConfig[type]} ${StylesConfig[size]} ${className}`);
 
   if (route) {
     return (
