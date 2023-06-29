ebba0ec522f51fba436c1d3f8395b71286935407
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-06-29 11:26:09 +0200

    We add the register form

--- src/app/dashboard/menu/page.tsx
+++ src/app/dashboard/menu/page.tsx
@@ -2,65 +2,69 @@
 import React, {useEffect,useState} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation'
 
 const Page = () => {
-
   const [username, setUserName] = useState("")
-
+  const router = useRouter()
+  
   useEffect(() => {
+
     let value
-    // Get the value from local storage if it exists
-    value = localStorage.getItem("username") || ""
-    setUserName(value)
+    if(!localStorage.getItem("username")){
+      router.push('/dashboard')
+    }else{
+      value = localStorage.getItem("username") || ""
+      setUserName(value)
+    }
+
   }, [])
 
-
-  return (
-    //<section className='bg-hg-500 w-full h-screen  flex flex-col justify-center items-center'>
-    <section className='bg-hg-200 w-full flex flex-col p-10 justify-center items-center'>
-        <p className='text-3xl'> Hola {username}! </p>
-        <p className='text-3xl'>Bienvenid@ a Holaglow</p>
-      <div className='grid grid-cols-3 p-5'>
-        <Link href='/dashboard/simulator3d'>
+    return (
+      <section className='bg-hg-200 w-full flex flex-col p-10 justify-center items-center'>
+          <p className='text-3xl'> Hola {username}! </p>
+          <p className='text-3xl'>Bienvenid@ a Holaglow</p>
+        <div className='grid grid-cols-3 p-5'>
+          <Link href='/dashboard/simulator3d'>
+            <div className='p-5'>
+              <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='3D Simulator' />
+              <p className='text-center text-lg'>Simulador 3D</p>
+            </div>
+          </Link>
           <div className='p-5'>
-            <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='3D Simulator' />
-            <p className='text-center text-lg'>Simulador 3D</p>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/budgets.png' height='50' width='190' alt='Pressupuestos' />
+            <p className='text-center text-lg'>Presupuestos</p>
           </div>
-        </Link>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/budgets.png' height='50' width='190' alt='Pressupuestos' />
-          <p className='text-center text-lg'>Presupuestos</p>
-        </div>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/financiar.png' height='50' width='190' alt='Financiar' />
-          <p className='text-center text-lg'>Financiación</p>
-        </div>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/checkout.png' height='50' width='190' alt='Pagar' />
-          <p className='text-center text-lg'>Pagar</p>
-        </div>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/agenda.png' height='50' width='190' alt='Agenda' />
-          <p className='text-center text-lg'>Agendar Cita</p>
-        </div>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='paciente' />
-          <p className='text-center text-lg'>Paciente</p>
-        </div>
-        <div className='p-5'>
-          <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='validar' />
-          <p className='text-center text-lg'>Validar</p>
-        </div>
-        <Link href='/dashboard'>
           <div className='p-5'>
-            <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='volver' />
-            <p className='text-center text-lg'>Logout</p>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/financiar.png' height='50' width='190' alt='Financiar' />
+            <p className='text-center text-lg'>Financiación</p>
           </div>
-        </Link>
-      </div>
-    </section>
- 
-  );
+          <div className='p-5'>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/checkout.png' height='50' width='190' alt='Pagar' />
+            <p className='text-center text-lg'>Pagar</p>
+          </div>
+          <div className='p-5'>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/agenda.png' height='50' width='190' alt='Agenda' />
+            <p className='text-center text-lg'>Agendar Cita</p>
+          </div>
+          <div className='p-5'>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='paciente' />
+            <p className='text-center text-lg'>Paciente</p>
+          </div>
+          <div className='p-5'>
+            <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='validar' />
+            <p className='text-center text-lg'>Validar</p>
+          </div>
+          <Link href='/dashboard'>
+            <div className='p-5'>
+              <Image className='mx-auto' src='/images/dashboard/menu-icons/3d_simulator.png' height='50' width='190' alt='volver' />
+              <p className='text-center text-lg'>Logout</p>
+            </div>
+          </Link>
+        </div>
+      </section>
+  
+    );
 };
 
 export default Page;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,10 +6,12 @@ import API_URLS from './config';
 
 export default function Page () {
 
-  
-
   const [error, setError] = useState('');
   const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    localStorage.clear(); // Limpiar localStorage al iniciar la app
+  }, []);
 
   const handleCheckUser = async () => {
 		try {
@@ -23,12 +25,12 @@ export default function Page () {
         window.location.href = '/dashboard/welcome';
       } else {
         localStorage.clear();
-        setError('Error al llamar a la API');
+        setError('Error autenticación');
       }
 		} catch (err) {
       localStorage.clear();
       console.log(err);
-      setError('Error de conexión');
+      setError('Error autenticación');
       setEmail('');
 		}
 	};
@@ -38,18 +40,60 @@ export default function Page () {
   };
 
   return (
-    <section className='bg-hg-200 h-screen flex flex-col justify-center items-center'>
-        <Image className='mx-auto p-10' src='/images/dashboard/holaglow_white.png' height='200' width='950' alt='Holaglow'/>
-        <div className="bg-gray-50 p-5">
-            <div className="mt-4">
-              <label htmlFor="email" className="text-2xl text-black">Email</label>
-                <div className="flex flex-col items-start">
-                    <input onChange={handleInputChange} type="email" value={email} name="email" className="block w-full mt-1 border-gray-400 rounded-md shadow-sm focus:border-indigo-300  text-black"/>
+    <section className='bg-hg-200 min-h-screen justify-center items-center'>
+      <div className='flex flex-wrap justify-center items-center'>
+        
+        <div className='w-full'>
+          <Image className='mx-auto m-10' src='/images/dashboard/holaglow_white.png' height='200' width='950' alt='Holaglow'/>
+        </div>
+
+        <div id="buscar" className='w-3/4'>
+          <div className="bg-gray-50 p-10 rounded-2xl flex">
+            
+            <div className="flex flex-col items-start flex-1">
+                <input onChange={handleInputChange} type="email" value={email} name="email" 
+                className="w-full h-full mt-1 border-2 border-gray rounded-md  text-black"
+                placeholder="Introduce tu teléfono, email o DNI"/>
+            </div>
+            <div className='flex flex-col flex-1'>
+            <button onClick={handleCheckUser} type="submit" className="inline-flex justify-center items-center w-full h-full px-5 py-3 ml-6 text-lg font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false">
+              Buscar</button>
+            </div>
+          </div>
+        </div>
+
+        <div id="Registro" className={`w-3/4 ${error ? '' : 'hidden'}`}>
+          <div className="flex justify-center items-center">
+            <div className="container">
+                <div className="w-full p-8 my-4 md:px-12 rounded-2xl shadow-2xl bg-white">
+                  <div className="flex">
+                      <h1 className="font-bold uppercase text-2xl text-black justify-center">¡Vaya! ¿Todavía no te conocemos?</h1>
+                  </div>
+                  <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                      <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        type="text" placeholder="Nombre*" />
+                      <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        type="text" placeholder="Primer Apellido*" />
+                      <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        type="text" placeholder="Segundo Apellido*" />
+                      <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        type="email" placeholder="Correo electrónico*" />
+                      <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        type="number" placeholder="Número de teléfono*" />
+                      
+                        <button className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 mt-2 rounded-lg w-full 
+                          focus:outline-none focus:shadow-outline">
+                            Continuar
+                        </button>
+                    
+                  </div>
+                
                 </div>
             </div>
-            <button onClick={handleCheckUser} type="submit" className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false">Register</button>
-            {error && <p className="text-red-500">{error}</p>} {/* Mostrar el mensaje de error */}
+          </div>
         </div>
+        
+      </div>
     </section>
  
   );
