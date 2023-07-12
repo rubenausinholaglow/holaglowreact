8fdb09aa5134dae24ee702c73653fe9f4477f523
Jordi <jordipuigdollers1992@gmail.com>
2023-07-12 11:59:05 +0200

    Dashboard (#35)
    * update next js and change paths
    
    * commit
    
    * Alma integration
    
    * commit
    
    * commit
    
    * changes
    
    * changes
    
    * Cambios
    
    * Cambios
    
    * minicart
    
    * minicart
    
    * fix lint errors
    
    ---------
    
    Co-authored-by: Ruben Ausin <rath1212@gmail.com>
    Co-authored-by: Lluís Tallada <lluis@thecolvinco.com>

--- src/app/dashboard/pages/welcome/page.tsx
+++ src/app/dashboard/pages/welcome/page.tsx
@@ -3,29 +3,39 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Page () {
-
-  const [username, setUserName] = useState("");
+export default function Page() {
+  const [username, setUserName] = useState('');
 
   useEffect(() => {
-    setUserName(localStorage.getItem("username") || "")
+    setUserName(localStorage.getItem('username') || '');
   }, []);
 
   return (
-    <section className='bg-hg-200 h-screen flex flex-col justify-center items-center'>
-      <div className='grid grid-cols-1 p-5'>
-        <p className='text-4xl'>¡Hola {username}!</p> 
+    <section className="bg-hg-200 h-screen flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 p-5">
+        <p className="text-4xl">¡Hola {username}!</p>
       </div>
-      <div className='grid grid-cols-1'>
-        <p className='text-4xl'>Bienvenid@ a
-        <Image className='mx-auto' src='/images/dashboard/holaglow_white.png' height='150' width='190' alt='Holaglow'/>
+      <div className="grid grid-cols-1">
+        <p className="text-4xl">
+          Bienvenid@ a
+          <Image
+            className="mx-auto"
+            src="/images/dashboard/holaglow_white.png"
+            height="150"
+            width="190"
+            alt="Holaglow"
+          />
         </p>
       </div>
-      <div className='grid grid-cols-1 p-5'>
-        <p className='text-4xl text-center py-5'>Preparad@ para ver<br/>tu glow?</p>
+      <div className="grid grid-cols-1 p-5">
+        <p className="text-4xl text-center py-5">
+          Preparad@ para ver
+          <br />
+          tu glow?
+        </p>
       </div>
-      <br/>
-        <Link href='/dashboard/pages/menu'>menu</Link>
+      <br />
+      <Link href="/dashboard/pages/menu">menu</Link>
     </section>
   );
-};
+}
