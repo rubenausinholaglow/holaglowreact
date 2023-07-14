fb5a05c8cd5a6f165f99b5672de7953c695c6057
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-07-14 08:40:16 +0200

    Changes detail

--- src/app/dashboard/(pages)/budgets/treatments/ProductCard.tsx
+++ src/app/dashboard/(pages)/budgets/treatments/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { Product } from '@interface/product';
 import Image from 'next/image';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 import { useCartStore } from '../stores/userCartStore';
 
@@ -12,7 +12,14 @@ export default function ProductCard({ product }: Props) {
   const addToCart = useCartStore(state => state.addItemToCart);
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl flex flex-col justify-between p-4 ">
-      <Link href={'/dashboard/budgets/treatments/detail/'}>
+      <Link
+        href={{
+          pathname: '/dashboard/budgets/treatments/detail',
+          query: {
+            search: product.id,
+          },
+        }}
+      >
         <Image
           src="/images/budget/promoCodeBg.jpg"
           alt={product.title}
@@ -20,23 +27,23 @@ export default function ProductCard({ product }: Props) {
           height={100}
           className="object-contain w-full h-40"
         />
-        <div className="flex-1 flex flex-col justify-between">
-          <h2 className="text-black text-lg font-semibold">{product.title}</h2>
-          <p className="text-gray-600 flex-1">{product.description}</p>
-          <div className="mt-4 flex items-center justify-between">
-            <span className="text-gray-800 font-semibold">
-              {product.price.toFixed(2)}€
-            </span>
-            <button
-              type="button"
-              className="ml-2 bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
-              onClick={() => addToCart(product)}
-            >
-              Añadir Producto
-            </button>
-          </div>
-        </div>
       </Link>
+      <div className="flex-1 flex flex-col justify-between">
+        <h2 className="text-black text-lg font-semibold">{product.title}</h2>
+        <p className="text-gray-600 flex-1">{product.description}</p>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="text-gray-800 font-semibold">
+            {product.price.toFixed(2)}€
+          </span>
+          <button
+            type="button"
+            className="ml-2 bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
+            onClick={() => addToCart(product)}
+          >
+            Añadir Producto
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/dashboard/pages/welcome/Welcome.tsx
+++
@@ -1,7 +0,0 @@
-import { Client } from '../../interface/client';
-
-export default function Header({ client }: { client: Client }) {
-  const username =
-    typeof window !== 'undefined' ? localStorage.getItem('username') : null;
-  return <p className="text-4xl">¡Hola {username}!</p>;
-}
