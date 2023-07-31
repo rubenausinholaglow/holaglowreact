b1f16fde75b428291563fc87e26920ccc665c7e4
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-07-31 15:49:59 +0200

    changes

--- src/app/dashboard/components/Filters.tsx
+++ src/app/dashboard/components/Filters.tsx
@@ -27,7 +27,6 @@ export const Filters: React.FC<FilterPageProps> = ({ onClickFilter }) => {
       layout="col-left"
       className={`sticky shrink-0 top-[10px] transition-all pr-8 mr-8 border-r border-hg-lightMalva`}
     >
-      <Timercomponent initialColor="green" />
       <SvgDoubleAngleLeft
         height={40}
         width={40}
