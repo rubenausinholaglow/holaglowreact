276617035946d6c590f2a65dcf3b20faa803803c
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-07-31 12:42:02 +0200

    change comments

--- src/app/dashboard/components/ui/Timer.tsx
+++ src/app/dashboard/components/ui/Timer.tsx
@@ -1,29 +1,79 @@
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import React, { useEffect, useState } from 'react';
 
-export function setupSocketConnection(): HubConnection {
-  const SOCKET_URL = process.env.NEXT_PUBLIC_CLINICS_API + '/Slack/Response';
-  const connection = new HubConnectionBuilder()
-    .withUrl(SOCKET_URL)
-    .withAutomaticReconnect()
-    .build();
-
-  connection
-    .start()
-    .then(() => {
-      connection.on('ReceiveMessage', Recivemessage => {
-        connection.stop().then(() => {
-          return null;
-        });
-      });
-    })
-    .catch(e => console.log('Connection failed: ', e));
-  return connection;
+interface TimerProps {
+  initialColor: string;
 }
 
-export function closeSocketConnection(connection: HubConnection) {
-  if (connection && connection.state === 'Connected') {
-    connection.stop().then(() => {
-      return null;
-    });
-  }
-}
+export const TimerComponent: React.FC<TimerProps> = ({ initialColor }) => {
+  const [color, setColor] = useState(initialColor);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    const updateColorAndTime = () => {
+      if (isRunning) {
+        const currentTimeStamp = new Date().getTime();
+        const totalTime = currentTimeStamp - startTime;
+
+        setCurrentTime(totalTime);
+
+        const fifteenMinutes = 15 * 60 * 1000;
+        const twentyFiveMinutes = 25 * 60 * 1000;
+
+        if (totalTime >= twentyFiveMinutes) {
+          setColor('red');
+        } else if (totalTime >= fifteenMinutes) {
+          setColor('yellow');
+        } else {
+          setColor('green');
+        }
+      }
+    };
+
+    updateColorAndTime();
+
+    const timer = setInterval(updateColorAndTime, 1000);
+
+    return () => clearInterval(timer);
+  }, [isRunning, startTime]);
+
+  const formatTime = (ms: number) => {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const handleStartTimer = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      setStartTime(new Date().getTime());
+    }
+  };
+
+  const handleStopTimer = () => {
+    setIsRunning(false);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          width: '50px',
+          height: '50px',
+          backgroundColor: color,
+          borderRadius: '50% 0 0 0',
+        }}
+      />
+      <div>{formatTime(currentTime)}</div>
+      <button onClick={handleStartTimer} disabled={isRunning}>
+        Start Timer
+      </button>
+      <button onClick={handleStopTimer} disabled={!isRunning}>
+        Stop Timer
+      </button>
+    </>
+  );
+};
+
+export default TimerComponent;
