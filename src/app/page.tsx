7847a062c604568ae5845d2da8229e8aa280978a
Lluís Tallada <lluis@thecolvinco.com>
2023-08-02 18:03:08 +0200

    disable prefetching in passport and budgets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Link href="/user/budget">budget</Link>
-      <Link href="/user/passport">passport</Link>
+      <Link prefetch={false} href="/user/budget">
+        budget
+      </Link>
+      <Link prefetch={false} href="/user/passport">
+        passport
+      </Link>
       <Link href="/form">form</Link>
       <Link href="/form">multistep</Link>
       <Link href="/dashboard" className="text-black">
