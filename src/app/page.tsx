6497e2b2d22e295249b01ac8bd3343bf59df63ca
Lluís Tallada <lluis@thecolvinco.com>
2023-08-09 17:42:22 +0200

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import { Flex } from 'components/Layouts/Layouts';
 
 export default function Home() {
-  return <Flex layout="col-center" className="text-center h-[1500px]"></Flex>;
+  return (
+    <Flex layout="col-center" className="text-center h-[1500px]">
+      <span />
+    </Flex>
+  );
 }
--- src/utils/IsMobile.ts
+++ src/utils/IsMobile.ts
@@ -3,8 +3,6 @@
 export default function IsMobile() {
   const breakpoint = window.document.querySelector('#breakpoint');
 
-  console.log(getComputedStyle(breakpoint, ':after').content);
-
   if (breakpoint) {
     return getComputedStyle(breakpoint, ':after').content === '"sm"';
   }
