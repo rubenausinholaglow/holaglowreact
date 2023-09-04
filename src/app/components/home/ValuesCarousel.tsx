ac7cd399711c7d6a9c899ff5fba7f170448250f2
Lluís Tallada <lluis@thecolvinco.com>
2023-09-04 12:37:38 +0200

    mobile homePAge

--- src/app/components/home/ValuesCarousel.tsx
+++ src/app/components/home/ValuesCarousel.tsx
@@ -12,7 +12,32 @@ const VALUES = [
 
 export default function ValuesCarousel() {
   return (
-    <ul className="bg-hg-lime w-full mb-12 text-center">
+    <div className="bg-hg-lime w-full overflow-hidden">
+      <div className="whitespace-nowrap overflow-hidden inline-block animate-horizontalScroll">
+        <ul className="inline-block">
+          {VALUES.map(value => (
+            <li className="inline-block pt-2 pb-1 px-6" key={value.name}>
+              <Text size="lg" className="font-medium">
+                {value.name}
+              </Text>
+            </li>
+          ))}
+        </ul>
+        <ul className="inline-block">
+          {VALUES.map(value => (
+            <li className="inline-block pt-2 pb-1 px-6" key={value.name}>
+              <Text size="lg" className="font-medium">
+                {value.name}
+              </Text>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+/*     <ul className="bg-hg-lime w-full mb-12 text-center">
       {VALUES.map(value => (
         <li className="inline-block py-3 px-6" key={value.name}>
           <Text size="lg" className="font-medium">
@@ -20,6 +45,4 @@ export default function ValuesCarousel() {
           </Text>
         </li>
       ))}
-    </ul>
-  );
-}
+    </ul> */
