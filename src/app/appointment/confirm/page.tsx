cf40ddd4f209f34852868455857b104af14eeabb
rubenausinholaglow <ruben.ausin@holaglow.com>
2023-09-05 10:34:34 +0200

    Fixes

--- src/app/appointment/confirm/page.tsx
+++ src/app/appointment/confirm/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect } from 'react';
-import { clearLocalStorage } from '@utils/utils';
 import { useRouter } from 'next/navigation';
 import ScheduleService from '@services/ScheduleService';
 
@@ -10,17 +9,17 @@ export default function Page({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const router = useRouter();
-
   const queryString = window.location.search;
   const params = new URLSearchParams(queryString);
   const appointmentId = params.get('id');
 
   useEffect(() => {
     const confirmAppointment = async () => {
-      var res = await ScheduleService.confirm(appointmentId!);
-      if (res) {
-        window.location.href = 'https://holaglow.com/appointment-confirmed';
+      if (appointmentId) {
+        const res = await ScheduleService.confirm(appointmentId);
+        if (res) {
+          window.location.href = 'https://holaglow.com/appointment-confirmed';
+        }
       }
     };
 
