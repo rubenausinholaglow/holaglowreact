37df90aed02046e73535b03bece69b264cd8c80c
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-09-08 09:18:19 +0200

    zIndex

--- src/app/dashboard/components/ui/Notification.tsx
+++ src/app/dashboard/components/ui/Notification.tsx
@@ -8,6 +8,7 @@ const Notification: React.FC<NotificationProps> = ({ message }) => {
   const [visible, setVisible] = useState<boolean>(true);
 
   useEffect(() => {
+    console.log(message);
     setVisible(true);
     const timer = setTimeout(() => {
       setVisible(false);
@@ -24,6 +25,7 @@ const Notification: React.FC<NotificationProps> = ({ message }) => {
       className={`fixed bottom-4 left-1/2 transform -translate-x-1/2 ${
         visible ? 'opacity-100' : 'opacity-0 pointer-events-none'
       } transition-opacity duration-300 bg-gray-800 text-white px-4 py-2 rounded-lg`}
+      style={{ zIndex: 9999 }}
     >
       <p>{message}</p>
     </div>
