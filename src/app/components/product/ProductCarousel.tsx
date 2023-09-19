32f10d551d270129f1574fc37e5a8b2c13841900
Lluís Tallada <lluis@thecolvinco.com>
2023-09-19 10:13:48 +0200

    testing slick slider

--- src/app/components/product/ProductCarousel.tsx
+++ src/app/components/product/ProductCarousel.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Slider from 'react-slick';
 import { Product } from '@interface/product';
 import { Carousel } from 'designSystem/Carousel/Carousel';
 import { Container, Flex } from 'designSystem/Layouts/Layouts';
+import { SvgSpinner } from 'icons/Icons';
 import { SvgArrow } from 'icons/IconsDs';
 
 import ProductCard from './ProductCard';
@@ -16,7 +17,13 @@ export default function ProductCarousel({
   className: string;
   products: Product[];
 }) {
-  const CONTAINER_WIDTH = 1280;
+  const [isSliderLoading, setIsSliderLoading] = useState(true);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [visibleProducts, setVisibleProducts] = useState(
+    products.filter(product => product.visibility).length
+  );
+
+  const CONTAINER_WIDTH = 1152;
   const CONTAINER_PADDING = 16;
 
   let CarouselWidth;
@@ -32,15 +39,24 @@ export default function ProductCarousel({
     CarouselWidth = document.body.clientWidth - 16;
   }
 
-  const visibleSlides = (CarouselWidth - (isCarouselExpanded ? 40 : 0)) / 304;
+  const firstLeftPadding =
+    (document.body.clientWidth - (CONTAINER_WIDTH - CONTAINER_PADDING * 2)) / 2;
 
-  const firstLeftPadding = (document.body.clientWidth - CONTAINER_WIDTH) / 2;
+  const visibleSlides = document.body.clientWidth / 304;
 
-  console.log(firstLeftPadding);
+  const customStyles = `.slick-list {padding-left: ${firstLeftPadding}px; padding-right: 80px}`;
 
-  const customStyles = '.slick-list {padding-left: 170px}';
+  const sliderRef = useRef<Slider | null>(null);
 
-  const sliderRef = useRef(null);
+  useEffect(() => {
+    setIsSliderLoading(true);
+
+    setVisibleProducts(products.filter(product => product.visibility).length);
+
+    setTimeout(() => {
+      setIsSliderLoading(false);
+    }, 350);
+  }, [products]);
 
   return (
     /*     <Carousel
@@ -69,49 +85,91 @@ export default function ProductCarousel({
       })}
     </Carousel> */
     <>
-      <style dangerouslySetInnerHTML={{ __html: customStyles }} />
+      {products.filter(product => product.visibility).length >
+        visibleSlides && (
+        <style dangerouslySetInnerHTML={{ __html: customStyles }} />
+      )}
 
-      <Slider
-        ref={sliderRef}
-        slidesToShow={visibleSlides}
-        infinite={false}
-        arrows={false}
-        swipeToSlide
-      >
-        {products.map((product, index) => {
-          if (product.visibility) {
-            return (
-              <ProductCard
-                key={product.id}
-                product={product}
-                className="h-full flex flex-col pr-10"
-              />
-            );
-          }
+      {isSliderLoading ? (
+        <Container className="py-28">
+          <SvgSpinner />
+        </Container>
+      ) : (
+        <>
+          {visibleProducts > visibleSlides ? (
+            <>
+              <Slider
+                ref={sliderRef}
+                slidesToShow={visibleSlides}
+                infinite={false}
+                arrows={false}
+                swipeToSlide
+                afterChange={index => setCurrentSlide(index)}
+                lazyLoad="progressive"
+              >
+                {products.map((product, index) => {
+                  if (product.visibility) {
+                    return (
+                      <ProductCard
+                        key={product.id}
+                        product={product}
+                        className="h-full flex flex-col pr-10"
+                      />
+                    );
+                  }
 
-          return null;
-        })}
-      </Slider>
-      <Container>
-        <Flex layout="row-right">
-          <div
-            className="transition-opacity bg-hg-secondary text-hg-primary rounded-full p-2 disabled:opacity-10 disabled:cursor-default"
-            onClick={() => {
-              sliderRef.current.slickPrev();
-            }}
-          >
-            <SvgArrow height={16} width={16} className="rotate-180" />
-          </div>
-          <div
-            className="transition-opacity bg-hg-secondary text-hg-primary rounded-full p-2 disabled:opacity-10 disabled:cursor-default"
-            onClick={() => {
-              sliderRef.current.slickNext();
-            }}
-          >
-            <SvgArrow height={16} width={16} />
-          </div>
-        </Flex>
-      </Container>
+                  return null;
+                })}
+              </Slider>
+              <Container>
+                <Flex layout="row-right" className="gap-6 my-12">
+                  <button
+                    className={`transition-opacity bg-hg-secondary text-hg-primary rounded-full p-2 ${
+                      currentSlide === 0 ? 'opacity-10' : 'cursor-pointer'
+                    }`}
+                    onClick={() => {
+                      sliderRef.current?.slickPrev();
+                    }}
+                  >
+                    <SvgArrow height={16} width={16} className="rotate-180" />
+                  </button>
+                  <button
+                    className={`transition-opacity bg-hg-secondary text-hg-primary rounded-full p-2 ${
+                      currentSlide ===
+                      products.filter(product => product.visibility).length
+                        ? 'opacity-10'
+                        : 'cursor-pointer'
+                    }`}
+                    onClick={() => {
+                      sliderRef.current?.slickNext();
+                    }}
+                  >
+                    <SvgArrow height={16} width={16} />
+                  </button>
+                </Flex>
+              </Container>
+            </>
+          ) : (
+            <Container className="mb-12">
+              <Flex layout="row-left" className="gap-10">
+                {products.map((product, index) => {
+                  if (product.visibility) {
+                    return (
+                      <ProductCard
+                        key={product.id}
+                        product={product}
+                        className="h-full flex flex-col w-[264px]"
+                      />
+                    );
+                  }
+
+                  return null;
+                })}
+              </Flex>
+            </Container>
+          )}
+        </>
+      )}
     </>
   );
 }
