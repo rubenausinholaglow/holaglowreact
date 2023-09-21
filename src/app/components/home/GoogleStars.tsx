cecb1cf35d4b4fe3218b59de6fcc11a878340154
Lluís Tallada <lluis@thecolvinco.com>
2023-09-21 13:21:54 +0200

    remove isWider param

--- src/app/components/home/GoogleStars.tsx
+++ src/app/components/home/GoogleStars.tsx
@@ -3,7 +3,7 @@ import { SvgGoogle, SvgStar } from 'icons/IconsDs';
 
 export default function GoogleStars() {
   return (
-    <Container isWider className="mb-2 md:mb-6">
+    <Container className="mb-2 md:mb-6">
       <Flex layout="row-center" className="gap-2 md:hidden ">
         <SvgStar className="-mt-1" />
         <span>4,7</span>
