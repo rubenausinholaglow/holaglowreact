ece6a66fb05da1fed74cd58f647cad2e68a9acc9
Lluís Tallada <lluis@thecolvinco.com>
2023-09-27 16:15:49 +0200

    checkout clinic layout

--- src/app/checkout/clinics/page.tsx
+++ src/app/checkout/clinics/page.tsx
@@ -9,7 +9,7 @@ import { useGlobalPersistedStore } from 'app/stores/globalStore';
 import { Container, Flex } from 'designSystem/Layouts/Layouts';
 import { Text, Title } from 'designSystem/Texts/Texts';
 import { SvgCheck } from 'icons/Icons';
-import { SvgAngle } from 'icons/IconsDs';
+import { SvgAngle, SvgCar, SvgRadioChecked } from 'icons/IconsDs';
 import { isEmpty } from 'lodash';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -32,10 +32,6 @@ export default function ClinicsCheckout() {
 
     if (isEmpty(clinics)) {
       initClinics();
-    }
-
-    if (isEmpty(selectedClinic) && !isEmpty(clinics)) {
-      setSelectedClinic(clinics[0]);
     }
   }, [clinics]);
 
@@ -60,38 +56,50 @@ export default function ClinicsCheckout() {
     router.push('/checkout/agenda');
   };
 
+  console.log(selectedClinic);
+
   return (
     <MainLayout isCheckout>
-      <div className="relative">
+      <div className="relative mt-9 md:mt-16">
         <Container className="md:pr-32">
           <Flex
             layout="col-left"
-            className="bg-hg-tertiary100 p-3 gap-3 rounded-xl md:w-1/2 mt-9"
+            className="bg-hg-tertiary100 p-3 gap-3 rounded-xl md:w-1/2 mb-12"
           >
-            <Flex layout="row-between" className="items-start">
+            <Flex layout="row-between" className="items-start w-full">
               <div>
-                <Text className="font-semibold text-left">
+                <Text className="font-semibold text-left mb-2">
                   Arrugas expresión: Frente, entrecejo y patas de gallo
                 </Text>
-                <Text className="text-left" size="sm">
+                <Text className="text-left" size="xs">
                   1 vial ácido hialurónico
                 </Text>
               </div>
-              <SvgCheck className="mt-[2px]" height={24} width={24} />
+              <SvgRadioChecked
+                className="mt-[2px] shrink-0"
+                height={24}
+                width={24}
+              />
             </Flex>
-            <Flex layout="col-right">
-              <Title className="text-hg-secondary mt-[12px] te">289€</Title>
-            </Flex>
+            <Text
+              size="xl"
+              className="text-hg-secondary font-semibold w-full text-right"
+            >
+              289€
+            </Text>
           </Flex>
 
-          <Title className="font-semibold">Selecciona tu clínica</Title>
+          <Title className="font-semibold mb-6">Selecciona tu clínica</Title>
 
-          <Flex layout="col-left" className="gap-4 mr-24 w-full md:w-1/2">
+          <Flex
+            layout="col-left"
+            className="gap-4 mr-24 w-full md:w-1/2 md:mb-8"
+          >
             {clinics.map((clinic, index) => (
               <Flex
                 layout="row-center"
-                className={`transition-all w-full justify-between p-4 cursor-pointer rounded-xl ${
-                  selectedClinic.city === clinic.city
+                className={`transition-all w-full justify-between p-3 cursor-pointer rounded-xl ${
+                  selectedClinic && selectedClinic.city === clinic.city
                     ? 'bg-hg-secondary100'
                     : 'bg-hg-black50'
                 } `}
@@ -106,43 +114,52 @@ export default function ClinicsCheckout() {
                     {clinic.address}
                   </address>
                 </Flex>
-                <SvgCheck className="mt-[2px]" height={24} width={24} />
+                {selectedClinic && selectedClinic.city === clinic.city ? (
+                  <SvgRadioChecked height={24} width={24} />
+                ) : (
+                  <div className="border border-hg-black h-[24px] w-[24px] rounded-full"></div>
+                )}
               </Flex>
             ))}
           </Flex>
-
           <Flex
-            layout="col-left"
-            className="bg-hg-primary300 p-3 gap-3 rounded-xl md:w-1/2 mt-9"
+            layout="row-left"
+            className="bg-hg-primary300 p-3 gap-3 rounded-t-2xl md:rounded-2xl md:w-1/2 items-start fixed bottom-0 left-0 right-0 md:relative"
           >
-            <Text size="lg" className="font-semibold mb-2">
-              Parking Graits
-            </Text>
-            <Text className="text-left" size="sm">
-              Te pagamos el parking para que tú disfrutes al máximo de la
-              experiencia
-            </Text>
-            <Link className="text-sm underline" href="https://www.google.cat">
-              Más info
-            </Link>
+            <div className="bg-hg-primary p-3 rounded-full">
+              <SvgCar height={16} width={16} />
+            </div>
+            <div>
+              <Text size="sm" className="font-semibold mb-2">
+                Parking Gratis
+              </Text>
+              <Text className="text-left" size="xs">
+                Te pagamos el parking para que tú disfrutes al máximo de la
+                experiencia
+              </Text>
+              <Link className="text-xs underline" href="https://www.google.cat">
+                Más info
+              </Link>
+            </div>
           </Flex>
+        </Container>
+
+        <div
+          id="mapLayer"
+          className="absolute bg-slate-400 top-0 bottom-0 right-0 left-1/2 hidden md:block"
+        >
           <div
-            id="mapLayer"
-            className="absolute bg-slate-400 top-0 bottom-0 right-0 left-1/2 hidden md:block"
+            className={`overflow-hidden max-w-full w-full`}
+            style={{ height: `${mapHeight}px` }}
           >
-            <div
-              className={`overflow-hidden max-w-full w-full`}
-              style={{ height: `${mapHeight}px` }}
-            >
-              <div id="g-mapdisplay" className="h-full w-full max-w-full">
-                <iframe
-                  className="h-full w-full border-none"
-                  src={`https://www.google.com/maps/embed/v1/place?q=Holaglow,+${googleMapAddress},+España&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}
-                ></iframe>
-              </div>
+            <div id="g-mapdisplay" className="h-full w-full max-w-full">
+              <iframe
+                className="h-full w-full border-none"
+                src={`https://www.google.com/maps/embed/v1/place?q=Holaglow,+${googleMapAddress},+España&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}
+              ></iframe>
             </div>
           </div>
-        </Container>
+        </div>
       </div>
 
       <Flex layout="col-center">
