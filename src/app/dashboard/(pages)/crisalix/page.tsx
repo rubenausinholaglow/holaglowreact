90fdac7bae58e30459947d3f80fc8836186331ba
Ruben Ausin <ruben.ausin@holaglow.com>
2023-09-29 17:33:33 +0200

    Crisalix implementation start

--- src/app/dashboard/(pages)/crisalix/page.tsx
+++ src/app/dashboard/(pages)/crisalix/page.tsx
@@ -1,40 +1,63 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import UserService from '@services/UserService';
 
 const Page = () => {
+  let playerId = '';
+  let playerToken = '';
+  useEffect(() => {
+    const userId = localStorage.getItem('id');
+    const appointmentId = localStorage.getItem('appointmentId');
+    const clinicId = localStorage.getItem('ClinicId');
+    UserService.createCrisalixUser(userId!, appointmentId!, clinicId!).then(
+      x => {
+        playerId = x.playerId;
+        playerToken = x.playerToken;
+        const toExecute = new Function(script);
+        toExecute();
+      }
+    );
+  }, []);
+  const script = `
+  var url = 'https://api3d.crisalix.com/v2/player.js';
+  var scriptLoaded = false;
+  var scripts = document.getElementsByTagName('script');
+  for (var i = scripts.length; i--; ) {
+    if (scripts[i].src == url && !scriptLoaded) scriptLoaded = true;
+  }
+  if(!scriptLoaded){
+    const scriptTag = document.createElement("script");
+    scriptTag.src = url;
+    document.head.appendChild(scriptTag);
+    scriptTag.addEventListener('load', function() {
+      var player_token = '${playerToken}';
+      var player = new CrisalixPlayer(player_token);
+      var options = {
+        container: document.getElementById('player'),
+        reconstruction_type: 'face',
+        player_id: '${playerId}'
+      };
+      options['locale'] = 'es';
+      player.render('patient', options);
+    });
+  }
+  else {
+    var player_token = '${playerToken}';
+    var player = new CrisalixPlayer(player_token);
+    var options = {
+      container: document.getElementById('player'),
+      reconstruction_type: 'face',
+      player_id: '${playerId}'
+    };
+    options['locale'] = 'es';
+    player.render('patient', options);
+  }
+`;
   return (
     <section className="bg-hg-pink h-screen flex flex-col items-center">
-      <p className="text-3xl p-10">Simulador 3D</p>
-      <div className="grid grid-cols-2">
-        <div>
-          <Image
-            className="mx-auto p-10"
-            src="/images/dashboard/menu-icons/3d_simulator.png"
-            height="50"
-            width="250"
-            alt="3D Simulator"
-          />
-          <p className="text-center text-lg">Subir Fotografías</p>
-        </div>
-        <div>
-          <Image
-            className="mx-auto p-10"
-            src="/images/dashboard/menu-icons/3d_simulator.png"
-            height="50"
-            width="250"
-            alt="3D Simulator"
-          />
-          <p className="text-center text-lg">Generar 3D</p>
-        </div>
-      </div>
-      <div className="fixed bottom-0 p-10">
-        <Link href="/dashboard/pages/menu">
-          <button className="bg-hg-secondary hover:bg-hg-tertiary text-white font-bold py-10 px-7 rounded-full">
-            Volver
-          </button>
-        </Link>
-      </div>
+      <div id="player"></div>
     </section>
   );
 };
