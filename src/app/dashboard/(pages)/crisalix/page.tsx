5485ab939672ae373ca5a9c430e30c254ab0f841
Ruben Ausin <ruben.ausin@holaglow.com>
2023-10-02 09:54:14 +0200

    Crisalix finished

--- src/app/dashboard/(pages)/crisalix/page.tsx
+++ src/app/dashboard/(pages)/crisalix/page.tsx
@@ -1,64 +1,132 @@
 'use client';
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 import UserService from '@services/UserService';
+import { Button } from 'designSystem/Buttons/Buttons';
+import MainLayout from 'app/components/layout/MainLayout';
+import { Container, Flex } from 'designSystem/Layouts/Layouts';
 
 const Page = () => {
-  let playerId = '';
-  let playerToken = '';
+  const [id, setId] = useState('');
+  const [playerId, setPlayerId] = useState('');
+  const [playerToken, setPlayerToken] = useState('');
+  const [simulationReady, setSimulationReady] = useState(false);
+  const [almostReady, setAlmostReady] = useState(false);
+  const [loadPlayer, setLoadPlayer] = useState(false);
+  const username =
+    typeof window !== 'undefined' ? localStorage.getItem('username') : null;
+
   useEffect(() => {
     const userId = localStorage.getItem('id');
     const appointmentId = localStorage.getItem('appointmentId');
     const clinicId = localStorage.getItem('ClinicId');
     UserService.createCrisalixUser(userId!, appointmentId!, clinicId!).then(
       x => {
-        playerId = x.playerId;
-        playerToken = x.playerToken;
-        const toExecute = new Function(script);
-        toExecute();
+        setPlayerId(x.playerId);
+        setPlayerToken(x.playerToken);
+        setId(x.id);
       }
     );
+
+    setTimeout(
+      () => {
+        setAlmostReady(true);
+      },
+      1 * 60 * 1000
+    );
   }, []);
-  const script = `
-  var url = 'https://api3d.crisalix.com/v2/player.js';
-  var scriptLoaded = false;
-  var scripts = document.getElementsByTagName('script');
-  for (var i = scripts.length; i--; ) {
-    if (scripts[i].src == url && !scriptLoaded) scriptLoaded = true;
-  }
-  if(!scriptLoaded){
-    const scriptTag = document.createElement("script");
-    scriptTag.src = url;
-    document.head.appendChild(scriptTag);
-    scriptTag.addEventListener('load', function() {
-      var player_token = '${playerToken}';
-      var player = new CrisalixPlayer(player_token);
-      var options = {
-        container: document.getElementById('player'),
-        reconstruction_type: 'face',
-        player_id: '${playerId}'
-      };
-      options['locale'] = 'es';
-      player.render('patient', options);
+
+  const checksimulationReady = () => {
+    if (id == '' || playerToken == '') return;
+    const clinicId = localStorage.getItem('ClinicId');
+    UserService.getSimulationReady(id, clinicId!).then(x => {
+      setSimulationReady(x);
+      if (!x) {
+        setTimeout(() => {
+          checksimulationReady();
+        }, 15 * 1000);
+      }
     });
-  }
-  else {
-    var player_token = '${playerToken}';
-    var player = new CrisalixPlayer(player_token);
-    var options = {
-      container: document.getElementById('player'),
-      reconstruction_type: 'face',
-      player_id: '${playerId}'
-    };
-    options['locale'] = 'es';
-    player.render('patient', options);
-  }
-`;
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      checksimulationReady();
+    }, 15 * 1000);
+  }, [playerId, playerToken]);
+
+  useEffect(() => {
+    if (playerId == '' || playerToken == '') return;
+    const script = `
+        var url = 'https://api3d.crisalix.com/v2/player.js';
+        var scriptLoaded = false;
+        var scripts = document.getElementsByTagName('script');
+        for (var i = scripts.length; i--; ) {
+          if (scripts[i].src == url && !scriptLoaded) scriptLoaded = true;
+        }
+        if(!scriptLoaded){
+          const scriptTag = document.createElement("script");
+          scriptTag.src = url;
+          document.head.appendChild(scriptTag);
+          scriptTag.addEventListener('load', function() {
+            var player_token = '${playerToken}';
+            var player = new CrisalixPlayer(player_token);
+            var options = {
+              container: document.getElementById('player'),
+              reconstruction_type: 'face',
+              player_id: '${playerId}'
+            };
+            options['locale'] = 'es';
+            player.render('patient', options);
+          });
+        }
+        else {
+          var player_token = '${playerToken}';
+          var player = new CrisalixPlayer(player_token);
+          var options = {
+            container: document.getElementById('player'),
+            reconstruction_type: 'face',
+            player_id: '${playerId}'
+          };
+          options['locale'] = 'es';
+          player.render('patient', options);
+        }
+      `;
+    const toExecute = new Function(script);
+    toExecute();
+  }, [loadPlayer]);
+
+  const startPlayer = () => {
+    setLoadPlayer(true);
+  };
+
   return (
-    <section className="bg-hg-pink h-screen flex flex-col items-center">
-      <div id="player"></div>
-    </section>
+    <MainLayout isDashboard>
+      {username && (
+        <Container>
+          <Flex layout="col-center">
+            <p className="font-bold text-4xl mb-2">
+              {username}, estamos generando tu 3D/Avatar...
+            </p>
+            {almostReady && (
+              <p className="font-bold text-4xl mb-2">
+                {username}, en breves podrás ver tu 3D
+              </p>
+            )}
+            {simulationReady && (
+              <div>
+                <p className="font-bold text-4xl mb-2">
+                  {username}, ¿Listo para ver tu 3D?
+                </p>
+
+                <Button size="lg" style="primary" onClick={startPlayer}>
+                  Ver 3D
+                </Button>
+              </div>
+            )}
+            {simulationReady && loadPlayer && <div id="player"></div>}
+          </Flex>
+        </Container>
+      )}
+    </MainLayout>
   );
 };
 
--- src/app/dashboard/services/UserService.ts
+++ src/app/dashboard/services/UserService.ts
@@ -16,6 +16,24 @@ export default class UserService {
     }
   }
 
+  static async getSimulationReady(id: string, clinicId: string) {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_CONTACTS_API}Crisalix/SimulationReady?id=${id}&clinic=${clinicId}`,
+        {
+          method: 'GET',
+        }
+      );
+      if (res.ok) {
+        const data = await res.json();
+        return data;
+      } else {
+        return null;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
   static async createCrisalixUser(
     userId: string,
     appointmentId: string,
