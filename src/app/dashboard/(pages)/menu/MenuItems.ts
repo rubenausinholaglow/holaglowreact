121c160ef2e068173075c452256f050fcfb8a302
Ruben Ausin <ruben.ausin@holaglow.com>
2023-10-02 10:42:03 +0200

    New crisalix testing

--- src/app/dashboard/(pages)/menu/MenuItems.ts
+++ src/app/dashboard/(pages)/menu/MenuItems.ts
@@ -1,10 +1,17 @@
+const newCrisalix = localStorage.getItem('newCrisalix');
+let crisalixUrl = 'https://sso.crisalix.com/users/sign_in';
+let target = '_blank';
+if (newCrisalix) {
+  crisalixUrl = '/dashboard/crisalix';
+  target = '';
+}
 export const menuItems = [
   {
     iconSrc: '/images/dashboard/menu-icons/3d_simulator.png',
     altText: 'Simulador 3D',
     title: 'Simulador 3D',
-    link: '/dashboard/crisalix',
-    target: '',
+    link: crisalixUrl,
+    target: target,
   },
   {
     iconSrc: '/images/dashboard/menu-icons/budgets.png',
