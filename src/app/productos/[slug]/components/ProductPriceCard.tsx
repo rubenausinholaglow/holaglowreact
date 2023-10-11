df8bfe39a84e98d960e39a19ddb165d24e66a7d0
Lluís Tallada <lluis@thecolvinco.com>
2023-10-11 14:32:56 +0200

    refactor pack logic and components to save a selectedPackOptions array to save it later

--- src/app/productos/[slug]/components/ProductPriceCard.tsx
+++ src/app/productos/[slug]/components/ProductPriceCard.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Product } from '@interface/product';
 import Dropdown from 'app/components/forms/Dropdown';
 import { useGlobalPersistedStore } from 'app/stores/globalStore';
@@ -15,6 +16,8 @@ import { Text } from 'designSystem/Texts/Texts';
 import * as icon from 'icons/IconsDs';
 import { SvgAdd, SvgArrow, SvgInjection, SvgMinus } from 'icons/IconsDs';
 import { isEmpty } from 'lodash';
+
+import ProductResults from './ProductResults';
 
 const UPGRADE_TYPES: Record<
   string,
@@ -78,7 +81,7 @@ const UPGRADE_TYPES: Record<
     title: 'BabyBotox',
     icon: 'Injection',
     options: [
-      { label: 'Baby botox', value: 'Baby botox' },
+      { label: 'BabyBotox', value: 'BabyBotox' },
       {
         label: 'Arrugas entrecejo y patas de gallo',
         value: 'Arrugas entrecejo y patas de gallo',
@@ -131,8 +134,153 @@ const UPGRADE_TYPES: Record<
 };
 
 export interface option {
-  label: string;
+  index: number;
   value: string;
+}
+
+function ProductPriceItemsCard({
+  product,
+  parentProduct,
+}: {
+  product: Product;
+  parentProduct: Product;
+}) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedPackOptions, setSelectedPackOptions] = useState(
+    [] as option[]
+  );
+
+  const updateSelectedPackOptions = (newValue: string, index: number) => {
+    const newOptions = [...selectedPackOptions];
+    const itemToUpdate = newOptions.find((item: any) => item.index === index);
+
+    if (itemToUpdate) {
+      itemToUpdate.value = newValue;
+    }
+
+    setSelectedPackOptions(newOptions);
+  };
+
+  const itemsIconsByIndex = product.packUnities.map((item, index) => {
+    const iconComponentName = `Svg${UPGRADE_TYPES[item.type.toString()].icon}`;
+    const IconComponent = (icon as any)[iconComponentName] || null;
+
+    return (
+      <IconComponent
+        key={index}
+        height={16}
+        width={16}
+        className="text-hg-secondary mr-2"
+      />
+    );
+  });
+
+  const defaultValues = product.packUnities.map((item, index) => {
+    const defaultValue =
+      index === 0
+        ? UPGRADE_TYPES[item?.type.toString()].options.find(
+            x => parentProduct.title == x.label
+          )
+        : UPGRADE_TYPES[item?.type.toString()].options[0];
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    const initialSelectedPackOptions = defaultValues.map((item, index) => {
+      return { index: index, value: item?.value };
+    });
+
+    setSelectedPackOptions(initialSelectedPackOptions);
+  }, []);
+
+  return (
+    <Flex layout="col-left" className="w-full">
+      {!product.isPack &&
+        product.packUnities.map((item: any, index: number) => {
+          return (
+            <Flex
+              key={UPGRADE_TYPES[item.type.toString()].title}
+              layout="row-left"
+            >
+              {itemsIconsByIndex[index]}
+              <Text className="text-sm md:text-md">
+                {UPGRADE_TYPES[item.type.toString()].title}
+              </Text>
+            </Flex>
+          );
+        })}
+
+      {product.isPack && (
+        <>
+          <form className="w-full" onSubmit={data => console.log(data)}>
+            {product.packUnities.map((item: any, index: number) => {
+              return (
+                <div
+                  className="w-full"
+                  key={UPGRADE_TYPES[item.type.toString()].title}
+                >
+                  <Flex layout="row-left">
+                    {itemsIconsByIndex[index]}
+                    <Text className="text-sm md:text-md">
+                      {UPGRADE_TYPES[item.type.toString()].title}
+                    </Text>
+                  </Flex>
+                  {showDropdown && (
+                    <Dropdown
+                      className="mt-2 w-full mb-4"
+                      options={UPGRADE_TYPES[item.type.toString()].options}
+                      defaultValue={defaultValues[index]}
+                      onChange={(value: any) => {
+                        updateSelectedPackOptions(value.value, index);
+                      }}
+                    />
+                  )}
+                </div>
+              );
+            })}
+
+            {product?.packMoreInformation && (
+              <Accordion>
+                <AccordionItem value="accordion">
+                  <AccordionContent>
+                    <p className="pl-5 pt-3 pb-0 text-sm md:text-md">
+                      {product?.packMoreInformation}
+                    </p>
+                  </AccordionContent>
+                  <AccordionTrigger>
+                    <span className="text-hg-secondary underline block text-left pt-3 pl-5 text-sm md:text-md">
+                      + info
+                    </span>
+                  </AccordionTrigger>
+                </AccordionItem>
+              </Accordion>
+            )}
+
+            {showDropdown && (
+              <Button
+                type="tertiary"
+                customStyles="bg-hg-primary"
+                className="mt-4"
+              >
+                Reservar cita
+                <SvgArrow height={16} width={16} className="ml-2" />
+              </Button>
+            )}
+          </form>
+
+          {!showDropdown && (
+            <Button
+              className="mt-4"
+              type="tertiary"
+              onClick={() => setShowDropdown(true)}
+            >
+              Seleccionar viales
+            </Button>
+          )}
+        </>
+      )}
+    </Flex>
+  );
 }
 
 export default function ProductPriceCard({
@@ -147,23 +295,7 @@ export default function ProductPriceCard({
   const { deviceSize, setSelectedTreatments } = useGlobalPersistedStore(
     state => state
   );
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedPackOptions, setSelectedPackOptions] = useState(
-    [] as option[]
-  );
-  const [isButtonDisabled, setIsButtonDisabled] = useState(product.isPack);
-  function selectedPackOption(i: number, x: option) {
-    var copy = selectedPackOptions;
-    if (selectedPackOptions.length < i + 1) {
-      selectedPackOptions.push(x);
-    } else {
-      selectedPackOptions[i] = x;
-    }
-    setSelectedPackOptions(copy);
-    if (selectedPackOptions.length == product.packUnities.length) {
-      setIsButtonDisabled(false);
-    }
-  }
+
   return (
     <Flex className="bg-white p-3 rounded-2xl w-full shadow-centered-secondary">
       <AccordionItem
@@ -236,86 +368,42 @@ export default function ProductPriceCard({
                 <Text size="sm">{product.description}</Text>
               </Flex>
             ) : (
-              <Flex layout="col-left" className="gap-1">
-                {product.packUnities.map((item: any, i: number) => {
-                  const iconComponentName = `Svg${
-                    UPGRADE_TYPES[item.type.toString()].icon
-                  }`;
-                  const IconComponent =
-                    (icon as any)[iconComponentName] || null;
-                  const options = UPGRADE_TYPES[item.type.toString()].options;
-                  let defaultValue = undefined;
-                  if (i == 0) {
-                    defaultValue = UPGRADE_TYPES[
-                      item.type.toString()
-                    ].options.find(x => parentProduct.title == x.label);
-                  }
-                  return (
-                    <Flex key={item.id} layout="col-left" className="w-full">
-                      <Flex layout="row-left">
-                        <IconComponent
-                          height={16}
-                          width={16}
-                          className="text-hg-secondary mr-2"
-                        />
-                        <Text className="text-sm md:text-md">
-                          {UPGRADE_TYPES[item.type.toString()].title}
-                        </Text>
-                      </Flex>
-
-                      {showDropdown && (
-                        <Dropdown
-                          className="mt-2 w-full mb-4"
-                          options={options}
-                          defaultValue={defaultValue}
-                          onChange={(x: any) => selectedPackOption(i, x)}
-                        />
-                      )}
-                    </Flex>
-                  );
-                })}
-              </Flex>
+              <ProductPriceItemsCard
+                product={product}
+                parentProduct={parentProduct}
+              />
             )}
 
-            {product?.packMoreInformation && (
-              <Accordion>
-                <AccordionItem value="accordion">
-                  <AccordionContent>
-                    <p className="pl-5 pt-3 pb-0 text-sm md:text-md">
-                      {product?.packMoreInformation}
-                    </p>
-                  </AccordionContent>
-                  <AccordionTrigger>
-                    <span className="text-hg-secondary underline block text-left pt-3 pl-5 text-sm md:text-md">
-                      + info
-                    </span>
-                  </AccordionTrigger>
-                </AccordionItem>
-              </Accordion>
-            )}
+            {product?.packMoreInformation && !product.isPack && (
+              <>
+                <Accordion>
+                  <AccordionItem value="accordion">
+                    <AccordionContent>
+                      <p className="pl-5 pt-3 pb-0 text-sm md:text-md">
+                        {product?.packMoreInformation}
+                      </p>
+                    </AccordionContent>
+                    <AccordionTrigger>
+                      <span className="text-hg-secondary underline block text-left pt-3 pl-5 text-sm md:text-md">
+                        + info
+                      </span>
+                    </AccordionTrigger>
+                  </AccordionItem>
+                </Accordion>
 
-            {product.isPack && !showDropdown ? (
-              <Button
-                type="tertiary"
-                className="mt-4"
-                onClick={() => setShowDropdown(!showDropdown)}
-              >
-                Seleccionar viales
-              </Button>
-            ) : (
-              <Button
-                disabled={isButtonDisabled}
-                type="tertiary"
-                customStyles="bg-hg-primary md:mt-4"
-                onClick={() => {
-                  setSelectedTreatments([product]);
-                }}
-                href={ROUTES.checkout.clinics}
-                className="mt-4"
-              >
-                Reservar cita
-                <SvgArrow height={16} width={16} className="ml-2" />
-              </Button>
+                <Button
+                  type="tertiary"
+                  customStyles="bg-hg-primary md:mt-4"
+                  onClick={() => {
+                    setSelectedTreatments([product]);
+                  }}
+                  href={ROUTES.checkout.clinics}
+                  className="mt-4"
+                >
+                  Reservar cita
+                  <SvgArrow height={16} width={16} className="ml-2" />
+                </Button>
+              </>
             )}
           </div>
         </AccordionContent>
