5105a1945fa7e7a8e657d3c964cfa5676bb7c871
Ruben Ausin <ruben.ausin@holaglow.com>
2023-10-18 16:13:11 +0200

    test

--- api/server.js
+++ api/server.js
@@ -1,33 +1,18 @@
 const express = require('express');
-const next = require('next');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const server = express();
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const server = express();
-
-  // Proxy the paths that should go to Framer
-  server.use(
-    '/landing/probador-virtual',
-    createProxyMiddleware({
-      target:
-        'https://practical-discussions-804147.framer.app/landing/probador-virtual/',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/landing/probador-virtual': '/', // if needed, you can modify the path here
-      },
-    })
+server.all('/landing/probador-virtual', (req, res) => {
+  // Redirect to your Framer URL or handle the proxy here
+  res.redirect(
+    'https://practical-discussions-804147.framer.app/landing/probador-virtual/'
   );
-
-  server.all('*', (req, res) => handle(req, res));
-
-  server.listen(3000, err => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
 });
 
-module.exports = app;
+server.all('*', (req, res) => {
+  // Handle other routes or simply forward to your Next.js app
+  res.send('Other routes here');
+});
+
+module.exports = (req, res) => {
+  return server(req, res);
+};
