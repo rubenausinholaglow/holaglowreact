fc6f1d21883050c99e237d7054f7d601454b9f1d
Lluís Tallada <lluis@thecolvinco.com>
2023-10-19 11:28:09 +0200

    video layout

--- src/app/tratamientos/[slug]/components/ProductInfo.tsx
+++ src/app/tratamientos/[slug]/components/ProductInfo.tsx
@@ -86,8 +86,13 @@ export default function ProductInfo({ product }: { product: Product }) {
           </Button>
         </Container>
         <div className="md:w-1/2">
-          <div className="relative aspect-[6/5] md:rounded-2xl">
-            <video autoPlay={true} loop={true} muted className="md:rounded-xl">
+          <div className="relative aspect-[4/5] md:rounded-2xl overflow-hidden">
+            <video
+              autoPlay={true}
+              loop={true}
+              muted
+              className="-translate-y-[25%]"
+            >
               <source src="/videos/pdp.mp4" type="video/mp4" />
             </video>
           </div>
