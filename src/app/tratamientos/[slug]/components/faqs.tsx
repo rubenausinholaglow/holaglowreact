7f9422d646d293f9067ca81b0a6a5967f9e277a8
Ruben Ausin <ruben.ausin@holaglow.com>
2023-10-19 19:21:46 +0200

    Faqs

--- src/app/tratamientos/[slug]/components/faqs.tsx
+++ src/app/tratamientos/[slug]/components/faqs.tsx
@@ -1,30 +1,226 @@
-export interface FAQ = {
+export type FAQ = {
   title: string;
   description: string;
 };
 export const faqItems: Record<string, FAQ[]> = {
-  '0.5-vial-inyectable-antiarrugas': [
+  '4 viales de ácido hialurónico': [
     {
       title: '¿Podré ver los resultados de manera instantánea?',
       description:
-        'Los resultados son inmediatos y visibles desde el primer momento, aunque pueden estar condicionados por la posible inflamación. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación.',
+        'Los resultados son inmediatos y visibles desde el primer momento, aunque pueden estar condicionados por la posible inflamación. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación',
     },
     {
       title: '¿Cuánto tiempo duran los resultados del ácido hialurónico?',
       description:
-        'En Holaglow defendemos una medicina estética que cuida y, para ello, la profesionalidad y la empatía son fundamentales. Todos nuestros doctores comparten el mismo compromiso: ponerse en tu piel, de manera literal y metafóricamente.',
+        'Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el ácido hialurónico se reabsorbe durante los seis o doce meses posteriores a su aplicación.',
     },
-  ],
-  'hydrafacial-deluxe': [
     {
       title: '¿La aplicación de ácido hialurónico es dolorosa?',
       description:
-        'En Holaglow defendemos una medicina estética que cuida y, para ello, la profesionalidad y la empatía son fundamentales. Todos nuestros doctores comparten el mismo compromiso: ponerse en tu piel, de manera literal y metafóricamente.',
+        'La aplicación de ácido hialurónico no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas. ',
     },
     {
       title: '¿Qué efectos secundarios puede tener el ácido hialurónico?',
       description:
-        'En Holaglow defendemos una medicina estética que cuida y, para ello, la profesionalidad y la empatía son fundamentales. Todos nuestros doctores comparten el mismo compromiso: ponerse en tu piel, de manera literal y metafóricamente.',
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del ácido hialurónico son muy leves y de corta duración. Los más comunes son la aparición de hematomas, moretones o inflamaciones. Por lo que podrás continuar con tu rutina sin complicaciones.',
     },
   ],
+  'Ácido desoxicólico (ADC)': [
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'No, el resultado óptimo del tratamiento se verá a los tres meses posteriores a la finalización de su aplicación.',
+    },
+    {
+      title:
+        '¿Cuánto tiempo duran los resultados del tratamiento para la papada?',
+      description:
+        'La duración de este tratamiento es permanente, si no hay un aumento brusco de peso.',
+    },
+    {
+      title: '¿La aplicación del tratamiento para la papada es dolorosa?',
+      description:
+        'Para realizar este tratamiento se aplicará anestesia con lido inyectada.',
+    },
+    {
+      title:
+        '¿Qué efectos secundarios puede tener el tratamiento para la papada?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves y de corta duración. Los más comunes son hinchazón, aparición de hematomas o sentir la zona un poco dolorida. Por lo que podrás continuar con tu rutina sin complicaciones.',
+    },
+  ],
+  'Cóctel de vitaminas + ácido hialurónico': [
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada.',
+    },
+    {
+      title: '¿Qué efectos secundarios tiene la mesoterapia?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves, como inflamaciones. Por tanto, podrás continuar con tu rutina sin complicaciones.',
+    },
+    {
+      title: '¿La aplicación de la mesoterapia es dolorosa?',
+      description:
+        'La aplicación de la mesoterapia no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas. ',
+    },
+  ],
+  'Hidroxiapatita cálcica': [
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Los resultados son visibles de manera progresiva, no son evidentes desde el primer momento. El resultado óptimo del tratamiento se verá durante los tres meses posteriores a su aplicación.',
+    },
+    {
+      title:
+        '¿Cuánto tiempo duran los resultados de la hidroxiapatita cálcica?',
+      description:
+        'Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, los resultados de la hidroxiapatita cálcica se desarrollan gradualmente durante los tres meses posteriores a su aplicación y su duración aproximada es de 12 meses.',
+    },
+    {
+      title: '¿La aplicación de hidroxiapatita cálcica es dolorosa?',
+      description:
+        'La aplicación de hidroxiapatita cálcica no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas.',
+    },
+    {
+      title: '¿Qué efectos secundarios puede tener la hidroxiapatita cálcica?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios de la hidroxiapatita cálcica son muy leves y de corta duración. Los más comunes son la aparición de hematomas, moretones o inflamaciones. Por lo que podrás continuar con tu rutina sin complicaciones.',
+    },
+  ],
+  'Hilos absorbibles de PDO': [
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Los resultados son inmediatos y visibles desde el primer momento, aunque pueden estar condicionados por la posible inflamación. El resultado óptimo del tratamiento se verá aproximadamente a los 3 meses de la aplicación, en función de lo que tarde la producción de colágeno.',
+    },
+    {
+      title: '¿Cuánto tiempo duran los resultados de los hilos tensores? ',
+      description:
+        'Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, los hilos tensores se reabsorben durante los 9  meses posteriores a su aplicación.',
+    },
+    {
+      title: '¿La aplicación de los hilos tensores es dolorosa?',
+      description:
+        'La colocación de los hilos puede ser algo dolorosa, por eso aplicamos anestesia local antes del tratamiento para minimizar la molestia al paciente.',
+    },
+    {
+      title: '¿Qué efectos secundarios pueden tener los hilos tensores?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios de los hilos tensores son leves y de corta duración. Los más comunes son la aparición de hematomas, moretones o inflamaciones. Por lo que podrás continuar con tu rutina sin complicaciones.',
+    },
+  ],
+  'Neuromodulador (1 vial de inyectable antiarrugas)': [
+    {
+      title: '¿Podré notar los resultados de manera instantánea?',
+      description:
+        'No, los resultados se empezarán a notar al cabo de 48 - 72 h, que es cuando empieza a hacer efecto el neuromodulador. El resultado óptimo del tratamiento será a las tres semanas de la aplicación.',
+    },
+    {
+      title: '¿Cuánto tiempo duran los resultados del neuromodulador? ',
+      description:
+        'Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el neuromodulador se reabsorbe durante los cuatro o seis meses posteriores a su aplicación.',
+    },
+    {
+      title: '¿La aplicación del neuromodulador es dolorosa?',
+      description:
+        'La aplicación del neuromodulador no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas.',
+    },
+    {
+      title: '¿Qué efectos secundarios puede tener el neuromodulador?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del neuromodulador son muy leves y de corta duración, tan solo una posible hinchazón en el lugar de la inyección. Por lo que podrás continuar con tu rutina sin complicaciones.',
+    },
+  ],
+  'Ondas electromagnéticas': [
+    {
+      title:
+        '¿Puedo continuar con mi vida habitual después del tratamiento de radiofrecuencia?',
+      description:
+        'Por supuesto. Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves, tan solo una posible inflamación y enrojecimiento. Por lo que podrás continuar con tu rutina sin complicaciones.',
+    },
+    {
+      title: '¿La aplicación del tratamiento de radiofrecuencia es dolorosa?',
+      description:
+        'La aplicación del tratamiento no es dolorosa. Tan solo algunos pacientes con mucha sensibilidad, podrían notar un calentamiento de la piel que podría ser molesto. En esos casos, recomendamos la toma de algún analgésico suave.',
+    },
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Inmediatamente después del procedimiento, los resultados serán sutiles, pero observarás la piel más firme y más tersa, con mejor elasticidad y mejor aspecto general. Aun así, para ver el resultado final, el paciente tiene que esperar de dos a tres meses para que se estire la piel y el crecimiento de colágeno sea completo.',
+    },
+  ],
+  'Principios activos específicos': [
+    {
+      title: '¿Puedo continuar con mi vida habitual después del tratamiento?',
+      description:
+        'Por supuesto, podrás continuar con tu rutina sin complicaciones ni rojeces.',
+    },
+    {
+      title: '¿La aplicación del tratamiento es dolorosa?',
+      description:
+        'La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal.',
+    },
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada.',
+    },
+  ],
+  'Solución química con ácidos': [
+    {
+      title: '¿Puedo continuar con mi vida habitual después del tratamiento?',
+      description:
+        'Por supuesto, podrás continuar con tu rutina sin complicaciones aunque pueden aparecer enrojecimiento o cambios de color temporales.',
+    },
+    {
+      title: '¿La aplicación del tratamiento es dolorosa?',
+      description:
+        'La aplicación del tratamiento no es dolorosa pero puede generar sensaciones de escozor y picor durante el procedimiento. No se aplica crema anestésica ya que dificultaría la penetración del peeling.',
+    },
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada.',
+    },
+  ],
+  'Preparación farmacéutica de principios activos, agentes inertes y vehículos':
+    [],
+  'Campana con ventosa': [
+    {
+      title: '¿Puedo continuar con mi vida habitual después del tratamiento?',
+      description:
+        'Por supuesto, podrás continuar con tu rutina sin complicaciones ni rojeces.',
+    },
+    {
+      title: '¿La aplicación del tratamiento es dolorosa?',
+      description:
+        'La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal.',
+    },
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Después de un solo tratamiento, obtendrás una piel más luminosa y un rostro más descansado. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada.',
+    },
+  ],
+  'Principios activos + Booster personalizable': [
+    {
+      title: '¿Podré ver los resultados de manera instantánea?',
+      description:
+        'Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada.',
+    },
+    {
+      title: '¿Qué efectos secundarios tiene el tratamiento?',
+      description:
+        'Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves, como inflamaciones. Por tanto, podrás continuar con tu rutina sin complicaciones.',
+    },
+    {
+      title: '¿La aplicación del tratamiento es dolorosa?',
+      description:
+        'La aplicación del tratamiento no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas.',
+    },
+  ],
+  Vitaminas: [],
+  'Vitaminas + fármaco antiandrogénico': [],
 };
