8761ebf46a6c0d87fe658ffdb0a06d7fb966d4ec
JordiPuigdo <jordipuigdollers1992@gmail.com>
2023-10-19 17:34:43 +0200

    commit

--- src/app/tratamientos/[slug]/components/faqs.tsx
+++ src/app/tratamientos/[slug]/components/faqs.tsx
@@ -27,4 +27,235 @@ export const faqItems: Record<string, FAQ[]> = {
         'En Holaglow defendemos una medicina estética que cuida y, para ello, la profesionalidad y la empatía son fundamentales. Todos nuestros doctores comparten el mismo compromiso: ponerse en tu piel, de manera literal y metafóricamente.',
     },
   ],
+  
+  '0,5 viales de inyectable antiarrugas': [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "No, los resultados se empezarán a ver al cabo de 48 - 72 h, que es cuando empieza a hacer efecto el inyectable antiarrugas. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del inyectable antiarrugas?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el inyectable antiarrugas se reabsorbe durante los cuatro o seis meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación del inyectable antiarrugas es dolorosa?",
+      description: "La aplicación del inyectable antiarrugas no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el inyectable antiarrugas?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del inyectable antiarrugas son muy leves y de corta duración, tan solo una posible..."
+    }
+  ],
+  "1 Hydrafacial deluxe": [
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada."
+    },
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto. A diferencia de otros tratamientos faciales que se realizan a mano, la varita mecanizada de Hydrafacial permite una limpieza profunda sin tener que aplicar apenas presión. Por tanto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada."
+    }
+  ],
+  "1 sesión de microneedling": [
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada."
+    },
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto. A diferencia de otros tratamientos faciales que se realizan a mano, la varita mecanizada de Hydrafacial permite una limpieza profunda sin tener que aplicar apenas presión. Por tanto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada."
+    }
+  ],
+  "3 viales de ácido hialurónico": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Los resultados son inmediatos y visibles desde el primer momento, aunque pueden estar condicionados por la posible inflamación. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del ácido hialurónico?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el ácido hialurónico se reabsorbe durante los seis o doce meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación de ácido hialurónico es dolorosa?",
+      description: "La aplicación de ácido hialurónico no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el ácido hialurónico?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del ácido hialurónico son muy leves y de corta duración. Los más comunes son la aparición de hematomas, moretones o inflamaciones. Por lo que podrás continuar con tu rutina sin complicaciones."
+    }
+  ],
+  "14 viales de ácido hialurónico": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Sí, los resultados son inmediatos, pero pueden estar condicionados por la hinchazón después del tratamiento. El resultado óptimo se verá a las tres semanas de su aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del ácido hialurónico?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el ácido hialurónico se reabsorbe a partir de los 12 meses posteriores a su aplicación, aproximadamente."
+    },
+    {
+      title: "¿La aplicación del ácido hialurónico es dolorosa?",
+      description: "El tratamiento puede ser molesto o doloroso. Por eso, siempre se aplicará anestesia local previamente."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el tratamiento?",
+      description: "Los efectos secundarios son leves y de corta duración, tan solo una posible hinchazón o hematomas en el lugar de la inyección. Quizá también algún bulto o irregularidad, durante los primeros días."
+    }
+  ],
+  "2 hydrafacial express": [
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos te recomendamos realizar sesiones de forma continuada."
+    },
+    {
+      title: "¿Puedo continuar con mi vida habitual después del tratamiento?",
+      description: "Por supuesto. A diferencia de otros tratamientos faciales que se realizan a mano, la varita mecanizada de Hydrafacial permite una limpieza profunda sin tener que aplicar apenas presión. Por tanto, podrás continuar con tu rutina sin complicaciones ni rojeces."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. Por tanto, en este caso no hará falta aplicar anestésico tópico para disfrutar de tu momento de cuidado personal."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada."
+    }
+  ],
+  "2 viales de inyectable antiarrugas": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "No, los resultados se empezarán a ver al cabo de 48 - 72 h, que es cuando empieza a hacer efecto el inyectable antiarrugas. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del inyectable antiarrugas?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el inyectable antiarrugas se reabsorbe durante los cuatro o seis meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación del inyectable antiarrugas es dolorosa?",
+      description: "La aplicación del inyectable antiarrugas no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el inyectable antiarrugas?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del inyectable antiarrugas son muy leves y de corta duración, tan solo una posible hinchazón en el lugar de la inyección. Por lo que podrás continuar con tu rutina sin complicaciones."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada."
+    },
+    {
+      title: "¿Qué efectos secundarios tiene la mesoterapia?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves, como inflamaciones. Por tanto, podrás continuar con tu rutina sin complicaciones."
+    },
+    {
+      title: "¿La aplicación de la mesoterapia es dolorosa?",
+      description: "La aplicación de la mesoterapia no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    }
+  ],
+  "2 viales de mesoterapia": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "No, los resultados se empezarán a ver al cabo de 48 - 72 h, que es cuando empieza a hacer efecto el inyectable antiarrugas. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del inyectable antiarrugas?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el inyectable antiarrugas se reabsorbe durante los cuatro o seis meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación del inyectable antiarrugas es dolorosa?",
+      description: "La aplicación del inyectable antiarrugas no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el inyectable antiarrugas?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del inyectable antiarrugas son muy leves y de corta duración, tan solo una posible hinchazón en el lugar de la inyección. Por lo que podrás continuar con tu rutina sin complicaciones."
+    },
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Después de un solo tratamiento, obtendrás un tono de piel más uniforme y radiante visible. De todos modos, para lograr resultados más duraderos de luminosidad e hidratación te recomendamos realizar sesiones de forma continuada."
+    },
+    {
+      title: "¿Qué efectos secundarios tiene la mesoterapia?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son leves, como inflamaciones. Por tanto, podrás continuar con tu rutina sin complicaciones."
+    },
+    {
+      title: "¿La aplicación de la mesoterapia es dolorosa?",
+      description: "La aplicación de la mesoterapia no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    }
+  ],
+  "1 vial de inyectable antiarrugas": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Los resultados del ácido hialurónico se perciben de manera inmediata, aunque pueden estar condicionados por la inflamación. Sin embargo, los resultados del inyectable antiarrugas se empezarán a ver al cabo de 48 - 72 h. El resultado óptimo del tratamiento completo se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del tratamiento?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, los productos aplicados se reabsorben durante seis o doce meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación del tratamiento es dolorosa?",
+      description: "La aplicación del tratamiento no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el tratamiento?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios son muy leves y de corta duración, tan solo una posible hinchazón en el lugar de la inyección. Por lo que podrás continuar con tu rutina sin complicaciones."
+    }
+  ],
+  "1 vial de ácido hialurónico": [
+    {
+      title: "¿Podré ver los resultados de manera instantánea?",
+      description: "Los resultados son inmediatos y visibles desde el primer momento, aunque pueden estar condicionados por la posible inflamación. El resultado óptimo del tratamiento se verá a las tres semanas de la aplicación."
+    },
+    {
+      title: "¿Cuánto tiempo duran los resultados del ácido hialurónico?",
+      description: "Cada uno de nosotros es único y también lo es nuestra piel. Generalmente, el ácido hialurónico se reabsorbe durante los seis o doce meses posteriores a su aplicación."
+    },
+    {
+      title: "¿La aplicación de ácido hialurónico es dolorosa?",
+      description: "La aplicación de ácido hialurónico no es dolorosa. De todos modos, el umbral del dolor es distinto para cada persona por lo que estaremos encantados de aplicar anestésico tópico, si lo deseas."
+    },
+    {
+      title: "¿Qué efectos secundarios puede tener el ácido hialurónico?",
+      description: "Lo mejor de este tratamiento es que al ser mínimamente invasivo los efectos secundarios del ácido hialurónico son muy leves y de corta duración. Los más comunes son la aparición de hematomas, moretones o inflamaciones. Por lo que podrás continuar con tu rutina sin complicaciones."
+    }
+  ]
+
+
 };
