85fb7702508994f71b8f9854ba172adf771fa3c7
Ruben Ausin <ruben.ausin@holaglow.com>
2023-10-20 09:44:37 +0200

    Improvements validations

--- src/app/dashboard/components/TextInputField.tsx
+++ src/app/dashboard/components/TextInputField.tsx
@@ -17,7 +17,7 @@ const TextInputField: React.FC<TextInputFieldProps> = ({
     if (!isEmpty(error)) {
       return 'url("/images/forms/error.svg") no-repeat center right 12px';
     }
-    if (value.length > 0 && hasNoValidation) {
+    if (value.length > 0) {
       return 'url("/images/forms/formCheck.svg") no-repeat center right 12px';
     }
 
@@ -31,7 +31,7 @@ const TextInputField: React.FC<TextInputFieldProps> = ({
         placeholder={placeholder ? placeholder : ''}
         className={twMerge(
           `border border-hg-black300 rounded-2xl px-4 py-2 mr-4 w-full text-hg-black h-16 focus:outline-none ${
-            value.length > 0 && hasNoValidation ? 'border-hg-black' : ''
+            value.length > 0 ? 'border-hg-black' : ''
           }`
         )}
         type="text"
