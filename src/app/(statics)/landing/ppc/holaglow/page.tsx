2977ce2be7b8c7c635d896f5cf3800d3eff3169b
Lluís Tallada <lluis@thecolvinco.com>
2023-10-25 17:25:30 +0200

    WIP

---
+++ src/app/(statics)/landing/ppc/holaglow/page.tsx
@@ -0,0 +1,6 @@
+import MainLayout from 'app/components/layout/MainLayout';
+import type { Metadata } from 'next';
+
+export default function Home() {
+  return <MainLayout>OLAKEASE LANDING</MainLayout>;
+}
