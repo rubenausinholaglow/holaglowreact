bfc8efdfd33768f8b8c2ed52d70cf47b2f68aeb0
Ruben Ausin <ruben.ausin@holaglow.com>
2023-11-07 11:06:18 +0100

    Remove performance

--- src/app/checkout/components/layout/CheckoutHeader.tsx
+++ src/app/checkout/components/layout/CheckoutHeader.tsx
@@ -16,21 +16,12 @@ export default function CheckoutHeader() {
         strategy="beforeInteractive"
         src="https://d2wy8f7a9ursnm.cloudfront.net/v7/bugsnag.min.js"
       ></Script>
-      <Script
-        id="bugsnagPerformance"
-        type="module"
-        strategy="beforeInteractive"
-      >
-        import BugsnagPerformance from
-        &apos;//d2wy8f7a9ursnm.cloudfront.net/v1.2.0/bugsnag-performance.min.js&apos;
-      </Script>
 
       <Script
         id="bugsnagStart"
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
-          __html: `Bugsnag.start({ apiKey: '8c6cd8495d37038715215411471cd7e6' });
-          BugsnagPerformance.start({ apiKey: '8c6cd8495d37038715215411471cd7e6' });`,
+          __html: `Bugsnag.start({ apiKey: '8c6cd8495d37038715215411471cd7e6' });`,
         }}
       ></Script>
       <Script
