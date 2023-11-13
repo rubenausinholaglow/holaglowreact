b484914ac327b1e89db9c1190dc4a71ca516c3eb
Ruben Ausin <ruben.ausin@holaglow.com>
2023-11-13 10:32:23 +0100

    Condiciones black friday

--- src/app/components/layout/MobileNavigation.tsx
+++ src/app/components/layout/MobileNavigation.tsx
@@ -147,6 +147,9 @@ export default function MobileNavigation({
               <a href="/aviso-legal">
                 <li>Términos y condiciones</li>
               </a>
+              <a href="/condiciones-black-friday">
+                <li>Condiciones Black Friday</li>
+              </a>
             </ul>
           </SimpleAccordion>
         </Flex>
