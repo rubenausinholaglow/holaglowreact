f8b85dc24bd168c461fe573b8a018b1aef586b3b
Ruben Ausin <ruben.ausin@holaglow.com>
2023-11-14 12:20:36 +0100

    hideLayout parameter

--- src/app/checkout/contactform/page.tsx
+++ src/app/checkout/contactform/page.tsx
@@ -13,6 +13,10 @@ import { SvgCalendar, SvgLocation } from 'icons/Icons';
 dayjs.locale(spanishConf);
 
 export default function ConctactForm() {
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+
+  const hideLayout = params.get('hideLayout') == 'true';
   const { selectedTreatments, selectedSlot, selectedDay, selectedClinic } =
     useSessionStore(state => state);
   const [selectedTreatmentsNames, setSelectedTreatmentsNames] = useState('');
@@ -26,57 +30,53 @@ export default function ConctactForm() {
       );
     }
   }, []);
-
-  return (
-    <MainLayout isCheckout>
-      <Container className="px-0 mt-6 md:mt-16">
-        <Flex layout="col-left" className="gap-8 md:gap-16 md:flex-row">
-          <div className="w-full md:w-1/2 bg-hg-black50 px-4 py-6 md:p-8 rounded-3xl">
-            <Flex layout="col-left" className="gap-4 mb-8">
-              <Title size="xl" className="font-semibold">
-                Reserva tu cita
-              </Title>
-              {localSelectedDay != undefined && (
-                <>
-                  <Text size="sm">
-                    Introduce tus datos de contacto para la cita de{' '}
-                    <span className="font-semibold w-full">
-                      {selectedTreatmentsNames}
-                    </span>
-                  </Text>
-                  {selectedClinic && (
-                    <Flex className="">
-                      <span>
-                        <SvgLocation />
-                      </span>
-                      <Text size="xs" className="w-full text-left pl-2">
-                        {selectedClinic.address}, {selectedClinic.city}
-                      </Text>
-                    </Flex>
-                  )}
-                  <Flex>
+  var content = (
+    <Container className="px-0 mt-6 md:mt-16">
+      <Flex layout="col-left" className="gap-8 md:gap-16 md:flex-row">
+        <div className="w-full md:w-1/2 bg-hg-black50 px-4 py-6 md:p-8 rounded-3xl">
+          <Flex layout="col-left" className="gap-4 mb-8">
+            <Title size="xl" className="font-semibold">
+              Reserva tu cita
+            </Title>
+            {localSelectedDay != undefined && (
+              <>
+                <Text size="sm">
+                  Introduce tus datos de contacto para la cita de{' '}
+                  <span className="font-semibold w-full">
+                    {selectedTreatmentsNames}
+                  </span>
+                </Text>
+                {selectedClinic && (
+                  <Flex className="">
                     <span>
-                      <SvgCalendar />
+                      <SvgLocation />
                     </span>
-                    <Text
-                      size="xs"
-                      className="w-full text-left pl-2 capitalize"
-                    >
-                      {localSelectedDay.format('dddd')},{' '}
-                      {localSelectedDay.format('D')} de{' '}
-                      {localSelectedDay.format('MMMM')}{' '}
-                      {selectedSlot?.startTime}
+                    <Text size="xs" className="w-full text-left pl-2">
+                      {selectedClinic.address}, {selectedClinic.city}
                     </Text>
                   </Flex>
-                </>
-              )}
-            </Flex>
+                )}
+                <Flex>
+                  <span>
+                    <SvgCalendar />
+                  </span>
+                  <Text size="xs" className="w-full text-left pl-2 capitalize">
+                    {localSelectedDay.format('dddd')},{' '}
+                    {localSelectedDay.format('D')} de{' '}
+                    {localSelectedDay.format('MMMM')} {selectedSlot?.startTime}
+                  </Text>
+                </Flex>
+              </>
+            )}
+          </Flex>
 
-            <RegistrationForm />
-          </div>
-          <div className="w-full md:w-1/2"></div>
-        </Flex>
-      </Container>
-    </MainLayout>
+          <RegistrationForm />
+        </div>
+        <div className="w-full md:w-1/2"></div>
+      </Flex>
+    </Container>
   );
+  if (hideLayout) {
+    return content;
+  } else return <MainLayout isCheckout>{content}</MainLayout>;
 }
