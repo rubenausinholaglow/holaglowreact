ff310d2d3ed2c9aff9c81914185385f940cddbf6
Ruben Ausin <ruben.ausin@holaglow.com>
2023-11-21 13:32:06 +0100

    Remove commented code

--- src/app/dashboard/(pages)/checkout/components/payment/paymentMethods/PaymentInput.tsx
+++ src/app/dashboard/(pages)/checkout/components/payment/paymentMethods/PaymentInput.tsx
@@ -186,10 +186,6 @@ export default function PaymentInput(props: Props) {
 
   const openPepper = () => {
     setShowPepperModal(true);
-    /*window.open(
-      'https://www.pepperspain.com/pepper/Page.aspx?__IDAPPLGN=3470',
-      '_blank'
-    );*/
   };
   async function addPayment(number: any) {
     setIsLoading(true);
