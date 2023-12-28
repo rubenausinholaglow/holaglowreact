44b036120b2f9fe77cad4ffb8757a6cc3ea9e274
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2023-12-28 10:41:20 +0100

    commmit

--- src/app/(web)/components/layout/DashboardLayout.tsx
+++ src/app/(web)/components/layout/DashboardLayout.tsx
@@ -68,7 +68,6 @@ export default function DashboardLayout({
       SocketService.getInstance({
         urlConnection: SOCKET_URL_COMMUNICATIONS,
         onReceiveMessage: message => {
-          debugger;
           if (
             message.event === EventTypes.PatientArrived ||
             (message.event === EventTypes.StartAppointment &&
