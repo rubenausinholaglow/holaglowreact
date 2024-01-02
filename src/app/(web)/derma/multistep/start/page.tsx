cb203542c2bc4ea15f3b5ba2311cd88e4715cde2
Ruben Ausin <ruben.ausin@holaglow.com>
2024-01-02 16:06:17 +0100

    Derma start

---
+++ src/app/(web)/derma/multistep/start/page.tsx
@@ -0,0 +1,59 @@
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+
+import { DermaFooter } from '../../../components/layout/DermaFooter';
+import DermaHeader from '../../../components/layout/DermaHeader';
+import HomeBlocksDerma from '../../../components/dermahome/HomeBlocksDerma';
+import { Container, Flex } from 'designSystem/Layouts/Layouts';
+import { Title, Text } from 'designSystem/Texts/Texts';
+import { Button } from 'designSystem/Buttons/Buttons';
+import { SvgArrow } from 'app/icons/IconsDs';
+
+export const metadata: Metadata = {
+  title: 'Holaglow - La nueva cara de la medicina estética',
+  description:
+    'Di adiós a los prejuicios y haz realidad tu propia idea de belleza con tratamientos estéticos eficaces',
+};
+
+export default function Home() {
+  return (
+    <main>
+      <DermaHeader />
+      <Container className="py-12 overflow-hidden">
+        <Flex
+          layout="col-left"
+          className="gap-4 items-center relative md:justify-center md:flex-row"
+        >
+          <Flex layout="col-left" className="relative z-10 md:w-1/2">
+            <Title
+              isAnimated
+              size="2xl"
+              className="text-hg-secondary font-bold mb-12 md:mb-6 lg:pr-[20%]"
+            >
+              Tu camino hacia una piel más saludable y feliz comienza aquí
+            </Title>
+            <Text isAnimated className="text-hg-black500 md:w-full md:text-lg">
+              Conectamos a personas con médicos experimentados para un
+              descubrimiento personalizado de productos y tratamientos para el
+              acné hasta el envejecimiento.
+            </Text>
+          </Flex>
+        </Flex>
+      </Container>
+
+      <Button
+        isAnimated
+        type="secondary"
+        size="xl"
+        className="mx-auto md:mx-0 mb-10"
+        href="https://derma.holaglow.com/derma/multistep/steps"
+        id={'tmevent_multistep_module'}
+      >
+        Empezar
+        <SvgArrow className="ml-4" height={24} width={24} />
+      </Button>
+      <DermaFooter />
+      <Analytics />
+    </main>
+  );
+}
---
+++ src/app/(web)/derma/multistep/steps/page.tsx
@@ -0,0 +1,161 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import {
+  SvgArrowSmallLeft,
+  SvgCheck,
+  SvgCircle,
+  SvgHolaglow,
+} from 'app/icons/Icons';
+import { HOLAGLOW_COLORS } from 'app/utils/colors';
+import { Button } from 'designSystem/Buttons/Buttons';
+import { Carousel } from 'designSystem/Carousel/Carousel';
+import { Flex } from 'designSystem/Layouts/Layouts';
+import { Text } from 'designSystem/Texts/Texts';
+import Image from 'next/image';
+
+import { MULTISTEP_QUESTIONS, MULTISTEP_TREATMENTS } from './mockedData';
+import RegistrationForm from 'app/(web)/components/common/RegistrationForm';
+import { Client } from '@interface/client';
+
+export default function Form() {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  const [categorySelected, setCategorySelected] = useState<
+    number | undefined
+  >();
+
+  const [client, setClient] = useState<Client>({
+    email: '',
+    phone: '',
+    phonePrefix: '',
+    name: '',
+    surname: '',
+    secondSurname: '',
+    termsAndConditionsAccepted: false,
+    receiveCommunications: false,
+    page: '',
+    externalReference: '',
+    analyticsMetrics: {
+      device: 0,
+      locPhysicalMs: '',
+      utmAdgroup: '',
+      utmCampaign: '',
+      utmContent: '',
+      utmMedium: '',
+      utmSource: '',
+      utmTerm: '',
+      treatmentText: '',
+      externalReference: '',
+      interestedTreatment: '',
+      treatmentPrice: 0,
+    },
+    interestedTreatment: '',
+    treatmentPrice: 0,
+  });
+  const redirectTo = (index: number) => {
+    if (categorySelected !== undefined) {
+      window.document.location.href =
+        MULTISTEP_TREATMENTS[categorySelected].treatments[index].landing;
+    }
+  };
+
+  const STEPS = 4;
+  const progressBarWith: number = activeSlideIndex * (100 / STEPS);
+
+  const goBack = (index: number) => {
+    setActiveSlideIndex(index - 1);
+  };
+
+  const sendEventTracking = (index: number) => {
+    window.parent.postMessage(
+      '(googleevent)multistep_step_' + index + '|',
+      'https://www.holaglow.com'
+    );
+  };
+
+  useEffect(() => {
+    sendEventTracking(1);
+  }, []);
+
+  return (
+    <>
+      <header className="py-4 border-b border-hg-black50 mb-6 relative">
+        <Flex layout="row-left" className="max-w-[624px] mx-auto px-4">
+          {activeSlideIndex > 0 && (
+            <div className="w-[30px] mr-2 sm:hidden">
+              <SvgArrowSmallLeft
+                height={30}
+                width={30}
+                fill={HOLAGLOW_COLORS['tertiary']}
+                className="cursor-pointer self-center"
+                onClick={() => goBack(activeSlideIndex)}
+              />
+            </div>
+          )}
+          <SvgHolaglow
+            width={125}
+            height={30}
+            fill={HOLAGLOW_COLORS['tertiary']}
+          />
+        </Flex>
+      </header>
+      <main
+        id="multistep"
+        className="max-w-[624px] mx-auto relative overflow-hidden text-hg-black"
+      >
+        <div className="px-4 mb-12">
+          <ul className="flex bg-hg-tertiary500/40 h-[4px] w-full rounded-full">
+            <li
+              className="h-[4px] rounded-full bg-hg-tertiary transition-all"
+              style={{ width: `${progressBarWith}%` }}
+            ></li>
+          </ul>
+        </div>
+
+        <Carousel
+          totalSlides={STEPS}
+          currentSlide={activeSlideIndex}
+          dragEnabled={false}
+          touchEnabled={false}
+        >
+          <div className="bg-white px-4">
+            Paso 1, validación de usuario
+            <section className="mb-6">
+              <Text size="xl" className="mb-2 font-semibold">
+                Primero, comencemos con tu información básica
+              </Text>
+              <Text>
+                Ten en cuenta que nuestros profesionales de la salud no podrán
+                recetar antibióticos orales o medicamentos, como Roaccutane y
+                Spironolactone, en línea, ya que esto requiere una consulta en
+                persona. Nuestros médicos solo pueden abordar preocupaciones
+                relacionadas con tu piel.
+              </Text>
+            </section>
+            <section>
+              <RegistrationForm
+                redirect={false}
+                hasContinueButton={false}
+                formData={client}
+                setClientData={setClient}
+              />
+            </section>
+          </div>
+        </Carousel>
+
+        {activeSlideIndex > 0 && (
+          <Button
+            className="mt-8 ml-3"
+            type="secondary"
+            onClick={() => goBack(activeSlideIndex)}
+          >
+            <Flex layout="row-left">
+              <SvgArrowSmallLeft height={20} width={20} />
+              <span className="ml-2">Atrás</span>
+            </Flex>
+          </Button>
+        )}
+      </main>
+    </>
+  );
+}
