185d78d9f037f3f798f88e5145f09b0243f32d87
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-01-12 08:20:58 +0100

    add params

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -8,6 +8,11 @@ export const metadata: Metadata = {
   description:
     'Di adiós a los prejuicios y haz realidad tu propia idea de belleza con tratamientos estéticos eficaces',
   openGraph: {
+    url: 'https://holaglowreact-git-dev-966-hola-glow.vercel.app/',
+    type: 'website',
+    title: 'Holaglow - La nueva cara de la medicina estética',
+    description:
+      'Di adiós a los prejuicios y haz realidad tu propia idea de belleza con tratamientos estéticos eficaces',
     images: ['/images/home/OGimagen_Holaglow.jpg'],
   },
 };
