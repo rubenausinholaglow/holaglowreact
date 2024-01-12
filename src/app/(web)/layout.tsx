0ec86f8f0543edc09e33e19878daaea958e48ff7
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-01-12 08:34:01 +0100

    delete the meta

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -25,10 +25,6 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
-        <meta
-          property="og:image"
-          content="/images/home/OGimagen_Holaglow.jpg"
-        />
       </Head>
       <head>
         {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
