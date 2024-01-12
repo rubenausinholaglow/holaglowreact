3a200cc128096d3795ef132834712bce1312970b
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-01-12 13:39:14 +0100

    commit

--- src/app/(web)/tratamientos/[slug]/page.tsx
+++ src/app/(web)/tratamientos/[slug]/page.tsx
@@ -1,54 +1,12 @@
 import MainLayout from 'app/(web)/components/layout/MainLayout';
-import { Metadata, ResolvingMetadata } from 'next';
 
 import ProductDetail from './components/ProductDetail';
-import { useProductMetadata } from './useProductMetadata';
-
-/*
-type Props = {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const { title, description } = useProductMetadata(params.slug);
-
-  return {
-    title: title,
-    description: description,
-    openGraph: {
-      url: 'https://holaglowreact-git-dev-966-hola-glow.vercel.app/',
-      type: 'website',
-      title: title,
-      description: description,
-      images: ['/images/home/OGimagen_Holaglow.jpg'],
-    },
-  };
-}
-*/
 
 export default function ProductPage({
   params,
 }: {
   params: { slug: string; isDashboard: boolean };
 }) {
-  const { title, description } = useProductMetadata(params.slug);
-
-  const metadata = {
-    title: title,
-    description: description,
-    openGraph: {
-      url: 'https://holaglowreact-git-dev-966-hola-glow.vercel.app/',
-      type: 'website',
-      title: title,
-      description: description,
-      images: ['/images/home/OGimagen_Holaglow.jpg'],
-    },
-  };
-
   return (
     <MainLayout>
       <ProductDetail params={params}></ProductDetail>
