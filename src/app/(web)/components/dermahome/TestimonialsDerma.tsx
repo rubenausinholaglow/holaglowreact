239fc79df6198fcbec1db93d10430bd79e7648df
Ruben Ausin <ruben.ausin@holaglow.com>
2024-01-16 11:40:53 +0100

    Fixes

--- src/app/(web)/components/dermahome/TestimonialsDerma.tsx
+++ src/app/(web)/components/dermahome/TestimonialsDerma.tsx
@@ -12,51 +12,35 @@ import { Title, Underlined } from 'designSystem/Texts/Texts';
 const TESTIMONIALS: Testimonial[] = [
   {
     city: 'Barcelona',
-    name: 'Yolanda Pérez',
+    name: 'Carmen Ausín',
     imgUrl: '/images/statics/landings/captacion/YolandaPérez.jpg',
     value: 4.7,
     testimonial:
-      'Trato espectacular de todos, en especial Roma por el tiempo dedicado a explicar con detalle cada tratamiento y dando consejos de cómo obtener el mejor resultado. Es genial la visualización 3D de cómo quedaría lo que decidas hacerte. El doctor dedica tiempo a explicar el procedimiento y el resultado ha sido genial. 100% recomendable!',
+      'Ya no sabía qué hacer con mi acné, hasta que descubrí la crema formulada!!! Ya llevo unos meses usándola y empiezo a ver cambios en mi piel, cosa que no había notado con ninguna otra crema… ya no me da vergüenza salir a la calle sin maquillaje! Gracias!!',
   },
   {
-    city: 'Madrid',
-    name: 'Mía GC',
+    city: 'Toledo',
+    name: 'Marta Gil',
     imgUrl: '/images/statics/landings/captacion/MíaGC.jpg',
     value: 4.7,
     testimonial:
-      'La atención y el trato al caso específico de cada persona son muy buenos, además de tener detalles muy guays como una bebida al llegar. Las recomendaciones son muy honestas y te sientes acompañada en todo momento.',
+      'Además de haberme ayudado mucho con mi piel, siempre me han apoyado, los médicos son muy amables y profesionales. Da gusto recibir asesoramiento de esta calidad desde casa!',
   },
   {
-    city: 'Barcelona',
-    name: 'Lluna Santiago',
+    city: 'Alicante',
+    name: 'Rubén Zamora',
     imgUrl: '/images/statics/landings/captacion/LlunaSantiago.jpg',
     value: 4.7,
     testimonial:
-      'Me he hecho los labios y ha sido increíble!!! Sin duda el mejor lugar en el que poder confiarse. Un trato maravilloso hacia los clientes, os lo recomiendo!!',
+      'He luchado contra la rosácea durante muchos años y he probado muchísimos médicos y métodos, incluso láseres, pero solo con la crema formulada estoy consiguiendo resultados realmente duraderos!! ',
   },
   {
-    city: 'Barcelona',
-    name: 'Noemí Clemente',
+    city: 'Tarragona',
+    name: 'Amalia Rodriguez',
     imgUrl: '/images/statics/landings/captacion/NoemiClemente.jpg',
     value: 4.7,
     testimonial:
-      'Hace 1 mes fui hacerme un estudio para hacerme un tratamiento facial, desde el minuto 1 el trato fue increíble. Me hizo el estudio Roma que fue súper cercano y claro con lo que necesitaba, tengo que decir que hace tiempo quería hacerme el tratamiento pero no encontraba a nadie que me asesorara con tanta confianza y seguridad. Ayer fui a hacerme el tratamiento y me puse en manos del Dr. Basart que me explicó todos los pasos del tratamiento. Ya tengo fecha.',
-  },
-  {
-    city: 'Madrid',
-    name: 'Carlos Correa',
-    imgUrl: '/images/statics/landings/captacion/CarlosCorrea.jpg',
-    value: 4.7,
-    testimonial:
-      '¡¡Muy buena experiencia!! Desde el detalle de la bebida, el equipo de recepción hasta el momento del tratamiento con la doctora. Iba con mucho miedo por ser mi primera vez y salgo muy contento!',
-  },
-  {
-    city: 'Madrid',
-    name: 'Julia Capozzi',
-    imgUrl: '/images/statics/landings/captacion/JuliaCapozzi.jpg',
-    value: 4.7,
-    testimonial:
-      'Llevo unos días desde mi tratamiento de aumento de labios en Holaglow y se merece 5 estrellas. Estoy súper satisfecha, los resultados son exactamente los que deseaba. El trato del médico ha sido muy cercano y profesional. Me he sentido bien, me ha transmitido confianza la marca desde el primer momento. Además, un par de días después te llega la llamada de la clínica para preguntarte si estás bien. Pequeños pero importantes detalles de calidad en atención al paciente. ¡Repetiré!',
+      'Estaba cansada de los videos de tiktok recomendando soluciones milagrosas que luego no funcionan… Tener una consulta con un dermatólogo y una crema formulada para mi me ha ayudado a simplificar mi rutina facial y a escuchar lo que realmente necesita mi piel. Lo recomiendo mucho!!!',
   },
 ];
 
