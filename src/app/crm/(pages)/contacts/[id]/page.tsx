50544326e68a65bed4d5da1b1374f4f8a0461e13
Siegharty <kcabrera866@gmail.com>
2024-01-24 12:39:05 -0300

    Adds header in contact details page

--- src/app/crm/(pages)/contacts/[id]/page.tsx
+++ src/app/crm/(pages)/contacts/[id]/page.tsx
@@ -1,5 +1,86 @@
 'use client';
+import React from 'react';
+import {
+  Tab,
+  TabPanel,
+  Tabs,
+  TabsBody,
+  TabsHeader,
+} from '@material-tailwind/react';
+import MainLayoutCRM from 'app/crm/components/layout/MainLayoutCRM';
+import { SvgCalendar, SvgMedicine, SvgPhone } from 'app/icons/Icons';
+import { useSessionStore } from 'app/stores/globalStore';
+import { Flex } from 'designSystem/Layouts/Layouts';
 
-export default function ContactDetailPage() {
-  return <></>;
+interface ContactDetailProps {
+  params: any;
+}
+
+export default function ContactDetailPage({ params }: ContactDetailProps) {
+  const { userLoginResponse } = useSessionStore(state => state);
+
+  const data = [
+    {
+      label: 'Tareas',
+      value: 'Tareas',
+      icon: <SvgPhone />,
+      desc: 'Hola',
+    },
+    {
+      label: 'Datos personales',
+      value: 'Datos personales',
+      icon: <SvgMedicine />,
+      desc: 'Hola1',
+    },
+    {
+      label: 'Comentarios',
+      value: 'Comentarios',
+      icon: <SvgPhone />,
+      desc: 'Hola2',
+    },
+    {
+      label: 'Llamadas',
+      value: 'Llamadas',
+      icon: <SvgPhone />,
+      desc: 'Hola3',
+    },
+    {
+      label: 'Citas',
+      value: 'Citas',
+      icon: <SvgCalendar />,
+      desc: 'Hola4',
+    },
+    {
+      label: 'Presupuestos',
+      value: 'Presupuestos',
+      icon: <SvgPhone />,
+      desc: 'Hola5',
+    },
+  ];
+
+  return (
+    <MainLayoutCRM>
+      <div className="rounded-xl bg-white ml-64 mt-2 mr-4 h-screen">
+        <Tabs value="Tareas">
+          <TabsHeader>
+            {data.map(({ label, value, icon, desc }) => (
+              <Tab key={value} value={value}>
+                <Flex layout="row-center" className="">
+                  {icon}
+                  {label}
+                </Flex>
+              </Tab>
+            ))}
+          </TabsHeader>
+          <TabsBody>
+            {data.map(({ value, desc }) => (
+              <TabPanel key={value} value={value}>
+                {desc}
+              </TabPanel>
+            ))}
+          </TabsBody>
+        </Tabs>
+      </div>
+    </MainLayoutCRM>
+  );
 }
