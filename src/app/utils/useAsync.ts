fbdc1ff9f08a9aa57b33f332b6e604c45e5ffc3f
Siegharty <kcabrera866@gmail.com>
2024-01-24 12:37:23 -0300

    Adds hook for api management

---
+++ src/app/utils/useAsync.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useAsync = <T>(apiCall: () => Promise<T>) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const getErrorMessage = (error: unknown) => {
+    if (error instanceof Error) return error.message;
+    return String(error);
+  };
+
+  const fetchData = async () => {
+    try {
+      const result = await apiCall();
+      setData(result);
+    } catch (error) {
+      setError(getErrorMessage(error));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, isLoading, error };
+};
+
+export default useAsync;
