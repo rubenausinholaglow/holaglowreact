17b8991a7a9538c84a13502ea76aeecf20b113da
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-01-26 12:50:00 +0100

    Add ContainerCRM

--- src/app/crm/(pages)/menu/page.tsx
+++ src/app/crm/(pages)/menu/page.tsx
@@ -1,14 +1,12 @@
+import ContainerCRM from 'app/crm/components/layout/ContainerCRM';
 import MainLayoutCRM from 'app/crm/components/layout/MainLayoutCRM';
 import LoginChecker from 'app/crm/components/login/LoginChecker';
-import { Flex } from 'designSystem/Layouts/Layouts';
 
 export default function MenuPage() {
   return (
     <MainLayoutCRM>
       <LoginChecker>
-        <Flex className="items-center justify-center flex-col p-4 ">
-          <div className="mb-4">Menú a CRM</div>
-        </Flex>
+        <ContainerCRM>Welcome !</ContainerCRM>
       </LoginChecker>
     </MainLayoutCRM>
   );
