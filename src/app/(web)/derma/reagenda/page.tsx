aec10c3d9a6fa8bffb9e29f2311adae5665359de
Lluís Tallada <lluis@thecolvinco.com>
2024-01-29 19:02:00 +0100

    refactor reagenda page

--- src/app/(web)/derma/reagenda/page.tsx
+++ src/app/(web)/derma/reagenda/page.tsx
@@ -1,9 +1,5 @@
 import Reagenda from 'app/(web)/reagenda/reagenda';
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  return <Reagenda searchParams={searchParams}></Reagenda>;
+export default function Page() {
+  return <Reagenda isDerma />;
 }
