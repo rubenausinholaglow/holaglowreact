c9ad7ef1a6352782a8cc7dca399286b27ae93130
Lluís Tallada <lluis@thecolvinco.com>
2024-01-31 13:25:03 +0100

    remove olakease

--- src/app/(web)/components/dermahome/FloatingBottomBar.tsx
+++ src/app/(web)/components/dermahome/FloatingBottomBar.tsx
@@ -92,7 +92,6 @@ export default function FloatingBottomBar({
               )}
             </Button>
           </div>
-          OLAKEASE
           <Button
             type="primary"
             size="xl"
