56160eacc8e6639db5e30fdb30f723a4ce96ea11
Siegharty <kcabrera866@gmail.com>
2024-01-31 08:04:46 -0300

    DEV-1039 Agrega mas mapeos para las tablas del detalle de contacto

--- src/app/crm/utils/mappingColumns.ts
+++ src/app/crm/utils/mappingColumns.ts
@@ -1,6 +1,17 @@
 import dayjs from 'dayjs';
 
-import { Task, TaskDataTableContact } from '../types/Contact';
+import {
+  Appointment,
+  AppointmentsDataTableContact,
+  Budget,
+  BudgetsDataTableContact,
+  Call,
+  CallDataTableContact,
+  Comment,
+  CommentDataTableContact,
+  Task,
+  TaskDataTableContact,
+} from '../types/Contact';
 
 export const mappingTasks = (contactDetailTasks: any) => {
   const model: TaskDataTableContact[] = [];
@@ -12,6 +23,50 @@ export const mappingTasks = (contactDetailTasks: any) => {
       endDateTask: dayjs(task.completedTime).format('DD-MM-YYYY HH:mm:ss'),
     };
     model.push(newModel);
+  });
+  return model;
+};
+
+export const mappingComments = (contactDetailComments: any) => {
+  const model: CommentDataTableContact[] = [];
+  contactDetailComments.forEach((comment: Comment) => {
+    const newModel: CommentDataTableContact = {
+      agent: comment.agent ? comment.agent.username : '',
+      creationDate: comment.creationDate,
+      text: comment.text,
+    };
+    model.push(newModel);
+  });
+  return model;
+};
+
+export const mappingCalls = (contactDetailCalls: any) => {
+  const model: CallDataTableContact[] = [];
+  contactDetailCalls.forEach((call: Call) => {
+    model.push(call);
+  });
+  return model;
+};
+
+export const mappingAppointments = (contactDetailAppointments: any) => {
+  const model: AppointmentsDataTableContact[] = [];
+  contactDetailAppointments.forEach((appointment: Appointment) => {
+    const newModel: AppointmentsDataTableContact = {
+      status: appointment.status,
+      city: appointment.clinic.city,
+      dateAppointment: appointment.date,
+      treatments: appointment.appointmentProducts[0].product.title,
+      startTimeAppointment: appointment.startTime,
+    };
+    model.push(newModel);
+  });
+  return model;
+};
+
+export const mappingBudgets = (contactDetailBudgets: any) => {
+  const model: BudgetsDataTableContact[] = [];
+  contactDetailBudgets.forEach((budget: Budget) => {
+    model.push(budget);
   });
   return model;
 };
