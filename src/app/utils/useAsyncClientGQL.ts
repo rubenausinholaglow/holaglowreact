a139d07e0cde5c9697676261ce0dda4aac4cbc69
Siegharty <kcabrera866@gmail.com>
2024-02-12 11:31:35 -0300

    DEV-1025 Agrega hook para hacer fetch a graphQL desde client

---
+++ src/app/utils/useAsyncClientGQL.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { DocumentNode } from '@apollo/client';
+import Bugsnag from '@bugsnag/js';
+import { createApolloClient } from 'lib/client';
+
+const client = createApolloClient(process.env.NEXT_PUBLIC_CONTACTS_API!, '');
+
+const useAsyncClientGQL = (query: DocumentNode) => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<Error | unknown>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async (query: DocumentNode) => {
+    try {
+      const { data } = await client.query({ query });
+      if (data) {
+        setData(data);
+      } else {
+        Bugsnag.notify('Error getting data');
+      }
+    } catch (error) {
+      setError(error);
+      console.error('Error fetching data:', error);
+      Bugsnag.notify('Error fetching data');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(query);
+  }, [query]);
+
+  return { data, error, isLoading };
+};
+
+export default useAsyncClientGQL;
