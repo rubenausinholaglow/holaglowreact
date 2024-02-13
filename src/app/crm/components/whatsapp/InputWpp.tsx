780ffbf603445bcefe890a4e7d0a7409d18cdbd8
Siegharty <kcabrera866@gmail.com>
2024-02-13 08:04:19 -0300

    DEV-1025 Remueve submit

--- src/app/crm/components/whatsapp/InputWpp.tsx
+++ src/app/crm/components/whatsapp/InputWpp.tsx
@@ -67,7 +67,7 @@ export default function InputWpp({ userId }: InputWppProps) {
         />
       </div>
 
-      <button type="submit" onClick={sendWhatsApp}>
+      <button onClick={sendWhatsApp}>
         <SvgSendMessage />
       </button>
     </div>
