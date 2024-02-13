126f8e457418544a38514e3cc85e494a1d8fd201
Ruben Ausin <ruben.ausin@holaglow.com>
2024-02-13 16:44:07 +0100

    Google tag manager

--- src/app/(web)/derma/layout.tsx
+++ src/app/(web)/derma/layout.tsx
@@ -18,34 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="max-h-screen h-full bg-white text-hg-black">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
-        />
-      </Head>
-      <head>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KMG5BW3L"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KMG5BW3L');`,
-          }}
-        ></Script>
-        <link rel="icon" href="/derma_favicon.ico" />
-      </head>
       <App>{children}</App>
     </html>
   );
