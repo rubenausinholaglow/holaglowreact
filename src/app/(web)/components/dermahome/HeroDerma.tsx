276d3cdbc18db4e2fdecfd9675ad5c3091a65629
Lluís Tallada <lluis@thecolvinco.com>
2024-02-15 12:47:02 +0100

    remove checkhydration

--- src/app/(web)/components/dermahome/HeroDerma.tsx
+++ src/app/(web)/components/dermahome/HeroDerma.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import CheckHydration from '@utils/CheckHydration';
 import { useSessionStore } from 'app/stores/globalStore';
 import {
   DERMA_HEADER_HEIGHT_DESKTOP,
@@ -21,91 +20,86 @@ export default function HeroDerma() {
   const HEADER_HEIGHT_CLASS = `-${HEADER_HEIGHT}px`;
 
   return (
-    <CheckHydration>
-      <div
-        className="bg-gradient-30deg from-[#23D9B74D] to-[#FFC7C74D] pt-16 overflow-hidden"
-        style={{ marginTop: HEADER_HEIGHT_CLASS }}
+    <div
+      className="bg-gradient-30deg from-[#23D9B74D] to-[#FFC7C74D] pt-16 overflow-hidden"
+      style={{ marginTop: HEADER_HEIGHT_CLASS }}
+    >
+      <Flex
+        layout="col-left"
+        className="md:flex-row md:items-end max-w-[1400px] mx-auto"
       >
+        <Image
+          src="/images/derma/home/homeDerma.png"
+          alt="Holaglow"
+          width={912}
+          height={894}
+          className="md:w-2/5"
+        />
         <Flex
-          layout="col-left"
-          className="md:flex-row md:items-end max-w-[1400px] mx-auto"
+          layout="col-center"
+          className="bg-derma-secondary100 md:bg-transparent relative w-full self-stretch md:justify-end "
         >
-          <Image
-            src="/images/derma/home/homeDerma.png"
-            alt="Holaglow"
-            width={912}
-            height={894}
-            className="md:w-2/5"
-          />
-          <Flex
-            layout="col-center"
-            className="bg-derma-secondary100 md:bg-transparent relative w-full self-stretch md:justify-end "
-          >
-            <Container className="pb-12 md:p-0 overflow-hidden">
-              <Flex layout="col-left" className="md:ml-8">
-                <Flex
-                  layout="row-left"
-                  className="mb-4 md:order-2 w-full md:justify-start gap-4 py-3"
+          <Container className="pb-12 md:p-0 overflow-hidden">
+            <Flex layout="col-left" className="md:ml-8">
+              <Flex
+                layout="row-left"
+                className="mb-4 md:order-2 w-full md:justify-start gap-4 py-3"
+              >
+                <Link
+                  href="https://es.trustpilot.com/review/derma.holaglow.com"
+                  className="flex gap-2 items-center mr-auto"
                 >
-                  <Link
-                    href="https://es.trustpilot.com/review/derma.holaglow.com"
-                    className="flex gap-2 items-center mr-auto"
+                  <Image
+                    src="/images/derma/home/trustPilotLogo.svg"
+                    alt="TrustPilot Derma by Holaglow"
+                    height={34}
+                    width={140}
+                    className="h-6 w-auto"
+                  />
+                  <span className="mt-2">4,5</span>
+                </Link>
+                <Text className="text-hg-black400 text-xs">
+                  Impulsado por Holaglow
+                </Text>
+              </Flex>
+              <Flex
+                layout="col-left"
+                className="gap-4 items-center relative md:justify-center md:flex-row"
+              >
+                <Flex layout="col-left" className="relative z-10">
+                  <TitleDerma
+                    size="3xl"
+                    className="text-derma-primary text-left mb-4"
                   >
-                    <Image
-                      src="/images/derma/home/trustPilotLogo.svg"
-                      alt="TrustPilot Derma by Holaglow"
-                      height={34}
-                      width={140}
-                      className="h-6 w-auto"
-                    />
-                    <span className="mt-2">4,5</span>
-                  </Link>
-                  <Text className="text-hg-black400 text-xs">
-                    Impulsado por Holaglow
+                    Cuidado facial personalizado
+                  </TitleDerma>
+                  <Text
+                    isAnimated
+                    className="text-hg-black500 md:w-full lg:text-lg mb-8 lg:mb-16  "
+                  >
+                    Reserva tu{' '}
+                    <b>consulta online con un dermatólogo estético</b> y
+                    encuentra el mejor tratamiento para tu piel sin salir de
+                    casa.
                   </Text>
-                </Flex>
-                <Flex
-                  layout="col-left"
-                  className="gap-4 items-center relative md:justify-center md:flex-row"
-                >
-                  <Flex layout="col-left" className="relative z-10">
-                    <TitleDerma
-                      size="3xl"
-                      className="text-derma-primary text-left mb-4"
-                    >
-                      Cuidado facial personalizado
-                    </TitleDerma>
-                    <Text
-                      isAnimated
-                      className="text-hg-black500 md:w-full lg:text-lg mb-8 lg:mb-16  "
-                    >
-                      Reserva tu{' '}
-                      <b>consulta online con un dermatólogo estético</b> y
-                      encuentra el mejor tratamiento para tu piel sin salir de
-                      casa.
-                    </Text>
 
-                    <Flex
-                      layout="row-center"
-                      className="w-full md:justify-start"
+                  <Flex layout="row-center" className="w-full md:justify-start">
+                    <Button
+                      type="derma"
+                      size="xl"
+                      className="lg:mb-12"
+                      href="/multistep/start"
+                      id="tmevent_derma_multistep_start"
                     >
-                      <Button
-                        type="derma"
-                        size="xl"
-                        className="lg:mb-12"
-                        href="/multistep/start"
-                        id="tmevent_derma_multistep_start"
-                      >
-                        Reserva tu videllamada
-                      </Button>
-                    </Flex>
+                      Reserva tu videllamada
+                    </Button>
                   </Flex>
                 </Flex>
               </Flex>
-            </Container>
-          </Flex>
+            </Flex>
+          </Container>
         </Flex>
-      </div>
-    </CheckHydration>
+      </Flex>
+    </div>
   );
 }
