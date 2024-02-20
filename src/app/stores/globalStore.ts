1cc56be4734540006bbd27c04fbd3e4c09df2c4b
Ruben Ausin <ruben.ausin@holaglow.com>
2024-02-20 13:13:39 +0100

    Fix

--- src/app/stores/globalStore.ts
+++ src/app/stores/globalStore.ts
@@ -181,7 +181,7 @@ export const useSessionStore = create(
     }),
     {
       name: 'session-storage',
-      version: 12,
+      version: 13,
       storage: createJSONStorage(() => sessionStorage),
     }
   )
@@ -265,7 +265,7 @@ export const useGlobalPersistedStore = create(
     }),
     {
       name: 'global-storage',
-      version: 36,
+      version: 37,
     }
   )
 );
