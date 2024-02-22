cf6759647c6637222444fece4957ae301777d56a
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-02-22 11:40:45 +0100

    commit

--- src/app/(web)/checkout/components/PaymentMethods.tsx
+++ src/app/(web)/checkout/components/PaymentMethods.tsx
@@ -136,129 +136,134 @@ export const PaymentMethods = ({
             setActivePaymentMethod(activePaymentMethod);
           }}
         >
-          {checkoutPaymentItems.map(method => (
-            <AccordionItem
-              key={method.key}
-              value={method.key}
-              className="bg-white py-6 px-8 rounded-xl w-full"
-            >
-              <AccordionTrigger className="text-left">
-                <Flex className="gap-2" onClick={() => scrollDown()}>
-                  <SvgRadioChecked
-                    height={24}
-                    width={24}
-                    className="shrink-0 hidden group-data-[state=open]:block"
-                  />
-                  <div className="border border-hg-black h-[24px] w-[24px] rounded-full shrink-0 group-data-[state=open]:hidden"></div>
-                  <Text>{method.label}</Text>
-                  <Flex className="ml-auto gap-2">
-                    {PAYMENT_ICONS[method.key as keyof typeof PAYMENT_ICONS] &&
-                      PAYMENT_ICONS[
+          {checkoutPaymentItems.map(method =>
+            method.key === 'alma' && isDerma ? null : (
+              <AccordionItem
+                key={method.key}
+                value={method.key}
+                className="bg-white py-6 px-8 rounded-xl w-full"
+              >
+                <AccordionTrigger className="text-left">
+                  <Flex className="gap-2" onClick={() => scrollDown()}>
+                    <SvgRadioChecked
+                      height={24}
+                      width={24}
+                      className="shrink-0 hidden group-data-[state=open]:block"
+                    />
+                    <div className="border border-hg-black h-[24px] w-[24px] rounded-full shrink-0 group-data-[state=open]:hidden"></div>
+                    <Text>{method.label}</Text>
+                    <Flex className="ml-auto gap-2">
+                      {PAYMENT_ICONS[
                         method.key as keyof typeof PAYMENT_ICONS
-                      ].map((icon: string) => (
-                        <Image
-                          src={`/images/dashboard/payment/${icon}`}
-                          height={32}
-                          width={56}
-                          key={icon}
-                          alt={method.label}
-                          className="ml-auto"
-                        />
-                      ))}
+                      ] &&
+                        PAYMENT_ICONS[
+                          method.key as keyof typeof PAYMENT_ICONS
+                        ].map((icon: string) => (
+                          <Image
+                            src={`/images/dashboard/payment/${icon}`}
+                            height={32}
+                            width={56}
+                            key={icon}
+                            alt={method.label}
+                            className="ml-auto"
+                          />
+                        ))}
+                    </Flex>
                   </Flex>
-                </Flex>
-              </AccordionTrigger>
-              <AccordionContent>
-                <Flex
-                  layout="col-left"
-                  className="mt-4 pt-5 border-t border-hg-black w-full"
-                >
-                  <Text className="mb-4">
-                    Serás redirigido/a a la pasarela para efectuar el pago.
-                  </Text>
-
-                  <Flex className="justify-between w-full bg-hg-black50 p-4 rounded-xl mb-8">
-                    <Text>Total pago</Text>
-                    <Text>
-                      {isDerma
-                        ? getTotalFromCart(
-                            cart,
-                            percentageDiscount,
-                            priceDiscount,
-                            manualPrice
-                          )
-                        : '49,00€'}
+                </AccordionTrigger>
+                <AccordionContent>
+                  <Flex
+                    layout="col-left"
+                    className="mt-4 pt-5 border-t border-hg-black w-full"
+                  >
+                    <Text className="mb-4">
+                      Serás redirigido/a a la pasarela para efectuar el pago.
                     </Text>
-                  </Flex>
 
-                  {method.key == 'alma' && selectedTreatments[0].price > 49 ? (
-                    <>
-                      {financialTimes.map(financialTime => (
-                        <div key={financialTime.key}>
-                          <Button
-                            id={isDerma ? 'tmevent_derma_step6' : ''}
-                            className="self-end"
-                            type="tertiary"
-                            customStyles={`gap-2 mb-4 ${
-                              isDerma
-                                ? 'bg-derma-primary border-none text-white'
-                                : 'bg-hg-primary'
-                            }`}
-                            onClick={() => {
-                              handlePaymentClick(
-                                PaymentBank.Alma,
-                                parseInt(financialTime.key)
-                              );
-                            }}
-                          >
-                            {isLoadingKey[financialTime.key] ? (
-                              <Flex className="w-20 justify-center">
-                                <SvgSpinner height={24} width={24} />
-                              </Flex>
-                            ) : (
-                              <>
-                                {financialTime.label}
-                                <SvgArrow height={16} width={16} />
-                              </>
-                            )}
-                          </Button>
-                        </div>
-                      ))}
-                      {errorMessage && (
-                        <p className="text-red-600"> {errorMessage} </p>
-                      )}
-                    </>
-                  ) : (
-                    <Button
-                      id={isDerma ? 'tmevent_derma_step6' : ''}
-                      className="self-end"
-                      type="tertiary"
-                      customStyles={`gap-2 ${
-                        isDerma
-                          ? 'bg-derma-primary border-none text-white'
-                          : 'bg-hg-primary'
-                      }`}
-                      onClick={() => {
-                        setIsLoadingButton(true);
-                        setActivePayment(PaymentBank.Alma);
-                      }}
-                    >
-                      {isLoadingButton ? (
-                        <Flex className="w-20 justify-center">
-                          <SvgSpinner height={24} width={24} />
-                        </Flex>
-                      ) : (
-                        <>
-                          Continuar
-                          <SvgArrow height={16} width={16} />
-                        </>
-                      )}
-                    </Button>
-                  )}
-                </Flex>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
+                    <Flex className="justify-between w-full bg-hg-black50 p-4 rounded-xl mb-8">
+                      <Text>Total pago</Text>
+                      <Text>
+                        {isDerma
+                          ? getTotalFromCart(
+                              cart,
+                              percentageDiscount,
+                              priceDiscount,
+                              manualPrice
+                            )
+                          : '49,00€'}
+                      </Text>
+                    </Flex>
+
+                    {method.key == 'alma' &&
+                    selectedTreatments[0].price > 49 ? (
+                      <>
+                        {financialTimes.map(financialTime => (
+                          <div key={financialTime.key}>
+                            <Button
+                              id={isDerma ? 'tmevent_derma_step6' : ''}
+                              className="self-end"
+                              type="tertiary"
+                              customStyles={`gap-2 mb-4 ${
+                                isDerma
+                                  ? 'bg-derma-primary border-none text-white'
+                                  : 'bg-hg-primary'
+                              }`}
+                              onClick={() => {
+                                handlePaymentClick(
+                                  PaymentBank.Alma,
+                                  parseInt(financialTime.key)
+                                );
+                              }}
+                            >
+                              {isLoadingKey[financialTime.key] ? (
+                                <Flex className="w-20 justify-center">
+                                  <SvgSpinner height={24} width={24} />
+                                </Flex>
+                              ) : (
+                                <>
+                                  {financialTime.label}
+                                  <SvgArrow height={16} width={16} />
+                                </>
+                              )}
+                            </Button>
+                          </div>
+                        ))}
+                        {errorMessage && (
+                          <p className="text-red-600"> {errorMessage} </p>
+                        )}
+                      </>
+                    ) : (
+                      <Button
+                        id={isDerma ? 'tmevent_derma_step6' : ''}
+                        className="self-end"
+                        type="tertiary"
+                        customStyles={`gap-2 ${
+                          isDerma
+                            ? 'bg-derma-primary border-none text-white'
+                            : 'bg-hg-primary'
+                        }`}
+                        onClick={() => {
+                          setIsLoadingButton(true);
+                          setActivePayment(PaymentBank.Alma);
+                        }}
+                      >
+                        {isLoadingButton ? (
+                          <Flex className="w-20 justify-center">
+                            <SvgSpinner height={24} width={24} />
+                          </Flex>
+                        ) : (
+                          <>
+                            Continuar
+                            <SvgArrow height={16} width={16} />
+                          </>
+                        )}
+                      </Button>
+                    )}
+                  </Flex>
+                </AccordionContent>
+              </AccordionItem>
+            )
+          )}
         </AccordionPrimitive.Root>
       )}
     </>
