ab1a6c3a0345966944d24c0c7a039e2ce0b1b428
Lluís Tallada <lluis@thecolvinco.com>
2024-02-23 15:00:20 +0100

    testing homeSSR

---
+++ src/app/(web)/emptyHome/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://holaglow.com'),
+  title: 'Holaglow - La nueva cara de la medicina estética',
+  description:
+    'Di adiós a los prejuicios y haz realidad tu propia idea de belleza con tratamientos estéticos eficaces',
+  openGraph: {
+    url: 'https://holaglowreact-git-dev-966-hola-glow.vercel.app/',
+    type: 'website',
+    title: 'Holaglow - La nueva cara de la medicina estética',
+    description:
+      'Di adiós a los prejuicios y haz realidad tu propia idea de belleza con tratamientos estéticos eficaces',
+    images: ['/images/home/OGimagen_Holaglow.jpg'],
+  },
+};
+
+export default function HomePage() {
+  return <p>HOME!</p>;
+}
