6945d340a30ba6999c80c0591e36dee027cd4874
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-02-26 16:40:19 +0100

    commit

--- src/app/types/dayAvailability.ts
+++ src/app/types/dayAvailability.ts
@@ -2,3 +2,8 @@ export interface DayAvailability {
   date: string;
   availability: boolean;
 }
+
+export interface MonthAvailabilityResponse {
+  dayAvailabilities : Array<DayAvailability>
+  totalTime : number;
+}
