2883c23a9fab20a2541eab8e5e12ba5d64474eb6
Lluís Tallada <lluis@thecolvinco.com>
2024-02-27 11:59:39 +0100

    WIP

---
+++ src/app/(web)/tratamientos/[slug]/components/ProductHeaderSSR.tsx
@@ -0,0 +1,78 @@
+import { AnimateOnViewport } from 'app/(web)/components/common/AnimateOnViewport';
+import CategoryIcon from 'app/(web)/components/common/CategoryIcon';
+import { SvgGlow } from 'app/icons/IconsDs';
+import { useSessionStore } from 'app/stores/globalStore';
+import { Product } from 'app/types/product';
+import {
+  getImageProductsCarousel,
+  getProductCardColor,
+  useImageProps,
+} from 'app/utils/common';
+import { Button } from 'designSystem/Buttons/Buttons';
+import { Carousel } from 'designSystem/Carousel/Carousel';
+import { Container, Flex } from 'designSystem/Layouts/Layouts';
+import { Text, Title } from 'designSystem/Texts/Texts';
+import { isEmpty } from 'lodash';
+import Image from 'next/image';
+
+import ProductHeaderImage from './ProductHeaderImage';
+
+export default function ProductHeaderSSR({ product }: { product: Product }) {
+  const validTypes = [3, 6, 7, 8];
+
+  return (
+    <>
+      <Container className="p-0 md:px-4 gap-4 md:gap-16 justify-between md:mb-16 flex flex-col md:flex-row">
+        <Container className="md:w-1/2 md:px-0 md:flex md:flex-col md:justify-center md:items-start">
+          <Title size="2xl" className="text-left font-bold mb-4">
+            {product.title}
+          </Title>
+
+          {validTypes.includes(product.type) ? (
+            <Text className="text-hg-black500 mb-4">
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: product?.extraInformation?.resultDescription,
+                }}
+              />
+            </Text>
+          ) : (
+            <Text className="text-hg-black500 mb-4">
+              {product.extraInformation?.resultDescription}
+            </Text>
+          )}
+
+          <Flex className="gap-2">
+            {product.category.map(category => {
+              return (
+                <Button
+                  key={category.name}
+                  type="tertiary"
+                  customStyles="border-none pl-1 mb-8"
+                >
+                  <CategoryIcon category={category.name} className="mr-2" />
+                  {category.name}
+                </Button>
+              );
+            })}
+          </Flex>
+        </Container>
+        <div className="md:w-1/2">
+          <ProductHeaderImage product={product} />
+        </div>
+      </Container>
+      {product.type === 3 && (
+        <Container>
+          <Text isAnimated className="text-hg-black500 mb-4 mt-8">
+            <p
+              className="mb-16"
+              dangerouslySetInnerHTML={{
+                __html: product?.detail,
+              }}
+            />
+          </Text>
+        </Container>
+      )}
+    </>
+  );
+}
