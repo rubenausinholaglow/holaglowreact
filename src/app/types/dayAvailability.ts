3169ab0a297502a83a06366a254eca27b4512f17
Ruben Ausin <ruben.ausin@holaglow.com>
2024-02-28 14:29:20 +0100

    Revert "Merge branch 'development' into DEV-1160_PDPClinicsImprovements"
    This reverts commit cd5755e572d4d4748475e537cc2e5f081da34e9e, reversing
    changes made to b37202d7f5e3be7531dc324773a616a85ecad597.

--- src/app/types/dayAvailability.ts
+++ src/app/types/dayAvailability.ts
@@ -2,8 +2,3 @@ export interface DayAvailability {
   date: string;
   availability: boolean;
 }
-
-export interface MonthAvailabilityResponse {
-  dayAvailabilities : Array<DayAvailability>
-  totalTime : number;
-}
