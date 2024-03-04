b77b90d60831af948df868177d359832c79ed85a
Siegharty <kcabrera866@gmail.com>
2024-03-04 05:43:56 -0300

    DEV-1110 Agrega video e imagenes en mensaje de cliente si aplica

--- src/app/crm/components/whatsapp/MessageRight.tsx
+++ src/app/crm/components/whatsapp/MessageRight.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
+  getFileExtensionByName,
   isAllowedExtensionImage,
   isAllowedExtensionVideo,
 } from 'app/crm/utils/fileUtils';
+import Image from 'next/image';
 
 interface MessageRightProps {
   clientMessage: string;
@@ -27,10 +29,12 @@ export default function MessageRight({
   return (
     <div className="flex justify-end mb-2 pr-3">
       <div className="rounded py-2 px-3" style={{ backgroundColor: '#E2F7CB' }}>
-        {isImage() && <img src={urlFile} width={100} height={100} />}
+        {isImage() && (
+          <Image alt="imageAgent" src={urlFile} width={100} height={100} />
+        )}
         {isVideo() && (
-          <video width={500} height={600} controls>
-            <source src={urlFile} type="video/webm"></source>
+          <video width={300} height={300} controls>
+            <source src={urlFile} type={`video/${getFileExtensionByName(urlFile)}`}></source>
           </video>
         )}
         <p className="text-sm mt-1">{clientMessage}</p>
