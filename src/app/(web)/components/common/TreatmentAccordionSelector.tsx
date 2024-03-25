37fa3f3c3c9e4d55c45346a6778ba02414842843
Ruben Ausin <ruben.ausin@holaglow.com>
2024-03-25 11:59:06 +0100

    Small fix

--- src/app/(web)/components/common/TreatmentAccordionSelector.tsx
+++ src/app/(web)/components/common/TreatmentAccordionSelector.tsx
@@ -256,6 +256,7 @@ export default function TreatmentAccordionSelector({
         setSelectedPack(product);
       } else {
         setSelectedTreatments([product]);
+        setSelectedPack(undefined);
       }
     }
   }
