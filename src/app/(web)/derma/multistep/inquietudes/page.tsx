61bb90fef3b5c2189b7e69ff3e1055e6b9e532d8
Lluís Tallada <lluis@thecolvinco.com>
2024-03-26 10:24:10 +0100

    WIP

---
+++ src/app/(web)/derma/multistep/inquietudes/page.tsx
@@ -0,0 +1,65 @@
+import DermaLayout from 'app/(web)/components/layout/DermaLayout';
+import { SvgCheckSquare } from 'app/icons/IconsDs';
+import { Container, Flex } from 'designSystem/Layouts/Layouts';
+import { Text, Title } from 'designSystem/Texts/Texts';
+import Image from 'next/image';
+
+export default function Inquietudes() {
+  return (
+    <div className="bg-derma-secondary100 min-h-screen">
+      <DermaLayout hideButton hideFooter>
+        <Container className="px-0 md:px-4 md:pt-12">
+          <div className="md:w-1/2 md:pr-8">
+            <ul className="flex bg-derma-primary500/20 h-[4px] w-full rounded-full mb-6">
+              <li className="h-[4px] rounded-full bg-derma-primary transition-all w-[25%]"></li>
+            </ul>
+          </div>
+        </Container>
+        <Container>
+          <Flex
+            layout="col-left"
+            className="w-full md:flex-row gap-6 md:gap-16"
+          >
+            <div>
+              <Image
+                alt="Dr. Basart"
+                src="/images/derma/multistep/Basart.png"
+                height={192}
+                width={192}
+                className="mx-auto w-24 mb-4"
+              />
+              <Text className="text-xs text-derma-primary500 mb-1">
+                Paso 1. Necesidades personales
+              </Text>
+              <Title className="text-derma-primary font-light mb-1">
+                Selecciona las inquietudes que te gustaría resolver en tu
+                consulta
+              </Title>
+              <Text className="text-sm text-hg-black500">
+                Elige tantas opciones como desees
+              </Text>
+            </div>
+
+            <ul className="flex flex-col gap-4 w-full">
+              {[
+                'Acné',
+                'Enrojecimiento / Rosácea',
+                'Melasma / Manchas',
+                'Dermatitis',
+                'No se lo que tengo',
+              ].map(item => (
+                <li
+                  className="rounded-xl bg-derma-secondary400 p-3 flex justify-between"
+                  key={item}
+                >
+                  {item}
+                  <SvgCheckSquare className="h-6 w-6" />
+                </li>
+              ))}
+            </ul>
+          </Flex>
+        </Container>
+      </DermaLayout>
+    </div>
+  );
+}
--- src/app/(web)/derma/multistep/start/page.tsx
+++ src/app/(web)/derma/multistep/start/page.tsx
@@ -16,58 +16,63 @@ export const metadata: Metadata = {
 };
 export default function StartMultistep() {
   return (
-    <CheckHydration>
+    <>
       <meta name="robots" content="noindex,follow" />
       <div className="bg-derma-secondary100 min-h-screen">
         <DermaLayout hideButton hideFooter>
           <Container className="px-0 md:px-4">
-            <Image
-              src="/images/derma/home/dermaStart.jpg"
-              alt="Holaglow"
-              width={780}
-              height={752}
-              className="rounded-t-2xl w-full"
-            />
-          </Container>
-          <Container id="tm_derma_step0">
             <Flex
               layout="col-left"
-              className="items-center relative md:justify-center md:flex-row md:mt-12"
+              className="md:flex-row w-full md:gap-12 xl:gap-16"
             >
-              <Flex layout="col-left" className="relative z-10 md:w-1/2 py-4">
-                <Title
-                  size="xldr"
-                  className="text-derma-primary font-light mb-4"
-                >
-                  Tu rutina facial personalizada por 99€
-                </Title>
+              <Image
+                src="/images/derma/home/dermaStart.jpg"
+                alt="Holaglow"
+                width={780}
+                height={752}
+                className="rounded-t-2xl w-full md:w-1/2"
+              />
+              <Container id="tm_derma_step0" className="md:w-1/2 md:px-0">
                 <Flex
                   layout="col-left"
-                  className="gap-2 w-full text-hg-black500 mb-8"
+                  className="items-center relative md:justify-center md:flex-row md:mt-12"
                 >
-                  <Text className="md:text-lg mb-2">
-                    Videollamada de diagnóstico con el médico. Te enviamos a
-                    casa tu rutina personalizada:
-                  </Text>
+                  <Flex layout="col-left" className="relative z-10 py-4">
+                    <Title
+                      size="xldr"
+                      className="text-derma-primary font-light mb-4"
+                    >
+                      Tu rutina facial personalizada por 99€
+                    </Title>
+                    <Flex
+                      layout="col-left"
+                      className="gap-2 w-full text-hg-black500 mb-8"
+                    >
+                      <Text className="md:text-lg mb-2">
+                        Videollamada de diagnóstico con el médico. Te enviamos a
+                        casa tu rutina personalizada:
+                      </Text>
 
-                  {[
-                    'Espuma limpiadora',
-                    'Crema hidratante de día para tu afección',
-                    'Protector solar',
-                    'Receta de la crema personalizada de noche <span className="text-xs">(a pedir en farmacia por 25-40€)</span>',
-                  ].map(item => (
-                    <Flex key={item}>
-                      <SvgCheckCircle className="shrink-0 mr-2" />
-                      <p dangerouslySetInnerHTML={{ __html: item }} />
+                      {[
+                        'Espuma limpiadora',
+                        'Crema hidratante de día para tu afección',
+                        'Protector solar',
+                        'Receta de la crema personalizada de noche <span className="text-xs">(a pedir en farmacia por 25-40€)</span>',
+                      ].map(item => (
+                        <Flex key={item}>
+                          <SvgCheckCircle className="shrink-0 mr-2" />
+                          <p dangerouslySetInnerHTML={{ __html: item }} />
+                        </Flex>
+                      ))}
                     </Flex>
-                  ))}
+                    <StartButton />
+                  </Flex>
                 </Flex>
-                <StartButton />
-              </Flex>
+              </Container>
             </Flex>
           </Container>
         </DermaLayout>
       </div>
-    </CheckHydration>
+    </>
   );
 }
