2c0e30e7a6441d6b392d95ee4968c3a653917339
Ruben Ausin <ruben.ausin@holaglow.com>
2024-04-04 12:29:24 +0200

    Improvements appointments

--- src/app/(web)/reagenda/components/AppointmentList.tsx
+++ src/app/(web)/reagenda/components/AppointmentList.tsx
@@ -56,7 +56,8 @@ export default function AppointmentList({
 
     const getAppointments = async () => {
       if (token) {
-        const res = await ScheduleService.next(token);
+        let res = await ScheduleService.next(token);
+        res = res.filter(x => !x.isPast || (x.isPast && showPast));
         setAppointments(res);
         setIsHydrated(true);
       }
@@ -107,96 +108,92 @@ export default function AppointmentList({
       </Text>
       <Flex layout="col-left" className="gap-4">
         {appointments.map(appointment => {
-          if (!appointment.isPast || (appointment.isPast && showPast)) {
-            const hideButtons = isDerma; //&& dayjs(appointment.startTime) < dayjs().add(3, 'day');
+          const hideButtons = isDerma; //&& dayjs(appointment.startTime) < dayjs().add(3, 'day');
 
-            return (
-              <Flex
-                layout="col-left"
-                key={appointment.id}
-                className={`${
-                  isDerma ? 'bg-derma-secondary300' : 'bg-hg-secondary100'
-                } gap-2 p-4 md:p-6 rounded-2xl w-full justify-between items-start`}
-              >
-                <Flex layout="row-left" className="gap-2 items-center mb-2">
-                  <Text className="font-semibold">
-                    {appointment.treatmentText}
-                  </Text>
-                  {!appointment.isPast || (
-                    <span className="bg-orange-600 p-1 text-white rounded-md text-xs">
-                      Cita no asistida
-                    </span>
-                  )}
-                  {!appointment.isCancelled || (
-                    <span className="bg-red-600 py-1 px-2 text-white rounded-md text-xs">
-                      Cita cancelada
-                    </span>
-                  )}
-                </Flex>
-                <Flex layout="row-left" className="">
-                  <SvgCalendar className="mr-2" />
-                  <div className="text-xs">
-                    {dayjs(appointment.startTime).format('DD')} de{' '}
-                    {dayjs(appointment.startTime).format('MMMM')} a las {}
-                    {dayjs(appointment.startTime).format('HH:mm')}
-                  </div>
-                </Flex>
-                {!isDerma && (
-                  <Flex layout="row-left">
-                    <SvgLocation height={16} width={16} className="mr-2" />
-                    {clinics.length > 0 && (
-                      <div className="text-xs">
-                        {
-                          clinics.filter(
-                            clinic => clinic.flowwwId == appointment.clinicId
-                          )[0].address
-                        }
-                      </div>
-                    )}
-                  </Flex>
+          return (
+            <Flex
+              layout="col-left"
+              key={appointment.id}
+              className={`${
+                isDerma ? 'bg-derma-secondary300' : 'bg-hg-secondary100'
+              } gap-2 p-4 md:p-6 rounded-2xl w-full justify-between items-start`}
+            >
+              <Flex layout="row-left" className="gap-2 items-center mb-2">
+                <Text className="font-semibold">
+                  {appointment.treatmentText}
+                </Text>
+                {!appointment.isPast || (
+                  <span className="bg-orange-600 p-1 text-white rounded-md text-xs">
+                    Cita no asistida
+                  </span>
                 )}
-
-                {!hideButtons && (
-                  <Flex layout="row-between" className="w-full mt-6">
-                    <Button
-                      size={isMobile ? 'sm' : 'md'}
-                      id="button-addon2"
-                      type={isDerma ? 'derma' : 'tertiary'}
-                      customStyles={!isDerma ? 'bg-hg-primary' : ''}
-                      onClick={() => {
-                        rescheduleAppointment(appointment);
-                      }}
-                    >
-                      <div>Reagendar</div>
-                    </Button>
-                    {appointment.treatment?.toUpperCase() !=
-                      process.env.NEXT_PUBLIC_PROBADOR_VIRTUAL_ID &&
-                      appointment.treatment?.toUpperCase() !=
-                        process.env.NEXT_PUBLIC_VISITA_EVALUACION_ID &&
-                      !appointment.isPast &&
-                      !appointment.isCancelled && (
-                        <>
-                          <Button
-                            size={isMobile ? 'sm' : 'md'}
-                            type="tertiary"
-                            id="button-addon2"
-                            onClick={() => {
-                              setAppointmentToCancel(appointment);
-                              setShowCancelModal(true);
-                            }}
-                          >
-                            {!cancelling && <div id="cancelText">Cancelar</div>}
-                            {cancelling && (
-                              <SvgSpinner height={24} width={24} />
-                            )}
-                          </Button>
-                        </>
-                      )}
-                  </Flex>
+                {!appointment.isCancelled || (
+                  <span className="bg-red-600 py-1 px-2 text-white rounded-md text-xs">
+                    Cita cancelada
+                  </span>
                 )}
               </Flex>
-            );
-          }
+              <Flex layout="row-left" className="">
+                <SvgCalendar className="mr-2" />
+                <div className="text-xs">
+                  {dayjs(appointment.startTime).format('DD')} de{' '}
+                  {dayjs(appointment.startTime).format('MMMM')} a las {}
+                  {dayjs(appointment.startTime).format('HH:mm')}
+                </div>
+              </Flex>
+              {!isDerma && (
+                <Flex layout="row-left">
+                  <SvgLocation height={16} width={16} className="mr-2" />
+                  {clinics.length > 0 && (
+                    <div className="text-xs">
+                      {
+                        clinics.filter(
+                          clinic => clinic.flowwwId == appointment.clinicId
+                        )[0].address
+                      }
+                    </div>
+                  )}
+                </Flex>
+              )}
+
+              {!hideButtons && (
+                <Flex layout="row-between" className="w-full mt-6">
+                  <Button
+                    size={isMobile ? 'sm' : 'md'}
+                    id="button-addon2"
+                    type={isDerma ? 'derma' : 'tertiary'}
+                    customStyles={!isDerma ? 'bg-hg-primary' : ''}
+                    onClick={() => {
+                      rescheduleAppointment(appointment);
+                    }}
+                  >
+                    <div>Reagendar</div>
+                  </Button>
+                  {appointment.treatment?.toUpperCase() !=
+                    process.env.NEXT_PUBLIC_PROBADOR_VIRTUAL_ID &&
+                    appointment.treatment?.toUpperCase() !=
+                      process.env.NEXT_PUBLIC_VISITA_EVALUACION_ID &&
+                    !appointment.isPast &&
+                    !appointment.isCancelled && (
+                      <>
+                        <Button
+                          size={isMobile ? 'sm' : 'md'}
+                          type="tertiary"
+                          id="button-addon2"
+                          onClick={() => {
+                            setAppointmentToCancel(appointment);
+                            setShowCancelModal(true);
+                          }}
+                        >
+                          {!cancelling && <div id="cancelText">Cancelar</div>}
+                          {cancelling && <SvgSpinner height={24} width={24} />}
+                        </Button>
+                      </>
+                    )}
+                </Flex>
+              )}
+            </Flex>
+          );
         })}
 
         {appointments.length == 0 && (
