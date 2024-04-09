045c307138ebd5bd48278982130691348094dd1b
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-04-09 10:27:23 +0200

    Commit

---
+++ src/app/utils/productLimitations.ts
@@ -0,0 +1,15 @@
+interface ProductLimitation {
+    id : string;
+}
+
+export const productLimitations : ProductLimitation[] = [
+    {id: '2115'},
+    {id: '862'},
+    {id: '863'},
+    {id: '864'},
+    {id: '2155'},
+    {id: '2147'},
+    {id: '2151'},
+    {id: '4127'},
+]
+
