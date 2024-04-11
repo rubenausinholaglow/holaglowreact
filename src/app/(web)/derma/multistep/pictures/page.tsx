595d3ae606d597b2ad718730fa58630aa223c30a
Lluis <lluistallada@holaglow.com>
2024-04-11 12:49:33 +0200

    button size in pictures step

--- src/app/(web)/derma/multistep/pictures/page.tsx
+++ src/app/(web)/derma/multistep/pictures/page.tsx
@@ -89,6 +89,7 @@ export default function Pictures() {
                   <Text className="text-derma-tertiary">Atrás</Text>
                 </Button>
                 <Button
+                  size="lg"
                   className={isDisabled ? 'pointer-events-none' : ''}
                   href={ROUTES.derma.multistep.extraInfo}
                   type={!isDisabled ? 'dermaDark' : 'disabled'}
