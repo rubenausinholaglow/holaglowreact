b0d34f60264188b7549053cd33475df837246049
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-04-15 09:29:20 +0200

    add Frakmenta Widget

--- src/app/(dashboard)/dashboard/(pages)/checkout/components/payment/paymentMethods/FrakmentaWidget.tsx
+++ src/app/(dashboard)/dashboard/(pages)/checkout/components/payment/paymentMethods/FrakmentaWidget.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useEffect } from 'react';
 import { Flex } from 'designSystem/Layouts/Layouts';
 import { Text } from 'designSystem/Texts/Texts';
 
@@ -8,6 +10,37 @@ export default function FrakmentaWidget({
   totalPrice: number;
   className?: string;
 }) {
+  useEffect(() => {
+    const toExecute = new Function(script);
+    toExecute();
+  }, []);
+
+  const script = `
+      var url = '/scripts/Frakmenta/widget.js';
+      var scriptLoaded = false;
+      if (!url) url = 'http://xxx.co.uk/xxx/script.js';
+      var scripts = document.getElementsByTagName('script');
+      
+      for (var i = scripts.length; i--; ) {
+        if (scripts[i].src == url && !scriptLoaded) scriptLoaded = true;
+      }
+      if (!scriptLoaded) {
+        const scriptTag = document.createElement("script");
+        scriptTag.src = url;
+        scriptTag.setAttribute("data-apikey", ${process.env.NEXT_PUBLIC_FRAKMENTA_APIKEY});
+        scriptTag.setAttribute("data-name", "widgetFK");
+        scriptTag.setAttribute("data-api-url", ${process.env.NEXT_PUBLIC_FRAKMENTA_URL});
+      
+        document.head.appendChild(scriptTag);
+        scriptTag.addEventListener('load', function() {
+          var widgets = frakmenta_init();
+          simulator();
+        });
+      } else {
+       
+      }
+    `;
+
   return (
     <div className={`w-full ${className ? className : ''}`}>
       <Flex layout="col-left" className="w-full gap-2 text-sm ">
