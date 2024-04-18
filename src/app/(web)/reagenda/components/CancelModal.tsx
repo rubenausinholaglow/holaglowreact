616f183cd7252940559a9af092b01a8db821f74d
Lluis <lluistallada@holaglow.com>
2024-04-18 16:27:25 +0200

    change button colors in reagenda, change bg color in reagenda appointment

--- src/app/(web)/reagenda/components/CancelModal.tsx
+++ src/app/(web)/reagenda/components/CancelModal.tsx
@@ -76,8 +76,7 @@ export default function CancelModal({
         <Flex layout="row-between" className="w-full">
           <Button
             className="cursor-pointer"
-            type={isDerma ? 'derma' : 'tertiary'}
-            customStyles={!isDerma ? 'bg-hg-primary' : ''}
+            type={isDerma ? 'derma' : 'primary'}
             onClick={() => {
               if (appointmentToCancel) {
                 cancelAppointment(appointmentToCancel);
@@ -86,7 +85,7 @@ export default function CancelModal({
           >
             Si, cancelar cita
           </Button>
-          <Button type="tertiary">Volver</Button>
+          <Button type="white">Volver</Button>
         </Flex>
       </Flex>
     </Modal>
