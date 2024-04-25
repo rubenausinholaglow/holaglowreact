c3485fe9e86d32f415e0879ef1ccc8ce8505d81d
Lluis <lluistallada@holaglow.com>
2024-04-25 13:07:41 +0200

    show PV card in PDP

--- src/app/(web)/tratamientos/[slug]/components/ProductPricesSSR.tsx
+++ src/app/(web)/tratamientos/[slug]/components/ProductPricesSSR.tsx
@@ -10,6 +10,9 @@ import { headers } from 'next/headers';
 const ProductPriceCard = dynamic(() => import('./ProductPriceCard'), {
   ssr: false,
 });
+
+const PVCard = dynamic(() => import('./PVCard'), { ssr: false });
+
 const ProductSessionGroupedPriceCard = dynamic(
   () => import('./ProductSessionGroupedPriceCard'),
   { ssr: false }
@@ -75,7 +78,6 @@ export default function ProductPricesSSR({ product }: { product: Product }) {
         <Title size="2xl" className="font-bold mb-6 md:mb-12">
           Pide cita o más información
         </Title>
-
         <CheckHydration>
           {!isSessionProduct && (
             <Flex
@@ -89,6 +91,8 @@ export default function ProductPricesSSR({ product }: { product: Product }) {
                   parentProduct={product}
                 />
               ))}
+
+              <PVCard />
             </Flex>
           )}
         </CheckHydration>
@@ -122,7 +126,6 @@ export default function ProductPricesSSR({ product }: { product: Product }) {
             </Flex>
           </Flex>
         )}
-
         {isSessionProduct && !isEmpty(groupedSessionProducts) && (
           <Flex
             layout="col-left"
