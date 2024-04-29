d92530144b8a5b2505487220b4a6feddc8586c74
Lluis <lluistallada@holaglow.com>
2024-04-29 12:04:17 +0200

    dashboard pricing table

--- src/app/(dashboard)/dashboard/(pages)/checkout/components/payment/paymentMethods/PepperWidget.tsx
+++ src/app/(dashboard)/dashboard/(pages)/checkout/components/payment/paymentMethods/PepperWidget.tsx
@@ -24,18 +24,29 @@ export default function PepperWidget({
   return (
     <div className={`w-full ${className ? className : ''}`}>
       <Flex layout="col-left" className="w-full gap-2 text-sm ">
-        <Flex className="w-full bg-hg-secondary100 rounded-lg font-semibold">
-          <p className=" py-1.5 px-3 w-1/2 text-left shrink-0">
-            Pago financiado
-          </p>
-          <p className="rounded-lg bg-white p-1.5 w-1/2 border border-hg-black">
+        <Flex className="w-full bg-hg-secondary100 rounded-lg">
+          <p className=" p-2 w-1/2 text-left shrink-0">Pago financiado</p>
+          <p className="rounded-lg bg-white p-2 w-1/2 border border-hg-black">
             {totalPrice} €
           </p>
         </Flex>
         {PEPPER_TABLE_CONFIG.periods.map((period, index) => (
-          <Flex className="w-full bg-hg-secondary100 rounded-lg" key={index}>
-            <p className="rounded-lg py-1.5 px-3 w-1/2 text-left">{period}</p>
-            <p className="rounded-lg bg-white p-1.5 w-1/2 border border-hg-black">
+          <Flex
+            className={`w-full bg-hg-secondary100 rounded-lg ${
+              PEPPER_TABLE_CONFIG.periods.length === index + 1
+                ? 'font-bold text-md bg-hg-secondary300'
+                : 'bg-hg-secondary100'
+            }`}
+            key={index}
+          >
+            <p className="rounded-lg py-2 px-3 w-1/2 text-left">{period}</p>
+            <p
+              className={`rounded-lg  p-2 w-1/2 border border-hg-black ${
+                PEPPER_TABLE_CONFIG.periods.length === index + 1
+                  ? 'bg-hg-secondary text-white'
+                  : 'bg-white'
+              }`}
+            >
               {`${priceFormat(totalPrice * PEPPER_TABLE_CONFIG.values[index])}`}{' '}
               €
             </p>
