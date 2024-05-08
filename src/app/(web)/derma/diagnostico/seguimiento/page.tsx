6a07e0aa55d7c812f3a03b1481d94bdec74c5cd9
Lluis <lluistallada@holaglow.com>
2024-05-08 12:52:51 +0200

    seguimiento page

---
+++ src/app/(web)/derma/diagnostico/seguimiento/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { DiagnosticData } from '@interface/derma/diagnosis';
+import { dermaService } from '@services/DermaService';
+import CheckHydration from '@utils/CheckHydration';
+import { useSessionStore } from 'app/stores/globalStore';
+
+import Seguimiento from './Seguimiento';
+
+export default function SeguimientoPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const { dermaPhone } = useSessionStore(state => state);
+  const [diagnosisData, setDiagnosisData] = useState<DiagnosticData | null>(
+    null
+  );
+  const index = searchParams.index;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response: DiagnosticData =
+        await dermaService.getDiagnosis(dermaPhone);
+      setDiagnosisData(response);
+    };
+    if (dermaPhone) fetchData();
+  }, [dermaPhone]);
+
+  console.log(diagnosisData);
+
+  if (!diagnosisData || index == null) {
+    return <></>;
+  }
+
+  return (
+    <CheckHydration>
+      <Seguimiento
+        userId={diagnosisData?.user?.id}
+        diagnostic={diagnosisData?.diagnostic[Number(index)]}
+      />
+    </CheckHydration>
+  );
+}
