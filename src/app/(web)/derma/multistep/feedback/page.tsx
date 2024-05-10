f248eba8b7a433e93441f0b211e0fb2eef6947ce
Lluis <lluistallada@holaglow.com>
2024-05-10 10:21:41 +0200

    WIP

--- src/app/(web)/derma/multistep/feedback/page.tsx
+++ src/app/(web)/derma/multistep/feedback/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 import ROUTES from '@utils/routes';
 import DermaLayout from 'app/(web)/components/layout/DermaLayout';
 import { SvgArrow, SvgStethoscope, SvgVerify } from 'app/icons/IconsDs';
@@ -21,13 +22,19 @@ export default function Feedback() {
             layout="col-left"
             className="md:flex-row w-full md:gap-12 xl:gap-16"
           >
-            <Image
-              alt="Dra. Sonsoles Espí"
-              src="/images/derma/multistep/Sonsoles.png"
-              height={192}
-              width={192}
-              className="mx-auto w-24 mb-4 md:ml-0"
-            />
+            <div className="relative rounded-3xl md:rounded-2xl w-full md:w-1/2 md:mt-12">
+              <Image
+                alt="Dra. Sonsoles Espí"
+                src="/images/derma/multistep/Sonsoles.png"
+                height={192}
+                width={192}
+                className="mx-auto w-24 mb-4 md:ml-0"
+              />
+              <Text className="text-xl md:text-3xl text-derma-primary font-light font-gtUltra mb-4">
+                ¡Entendido! Una crema efectiva para{' '}
+                <span className="font-semibold">melasma</span> contiene:
+              </Text>
+            </div>
 
             <div id="tm_derma_step0" className="md:w-1/2">
               <Flex
@@ -35,11 +42,6 @@ export default function Feedback() {
                 className="items-center relative md:justify-center md:flex-row md:mt-12"
               >
                 <Flex layout="col-left" className="relative z-10 py-4">
-                  <Text className="text-xl md:text-3xl text-derma-primary font-light font-gtUltra mb-4">
-                    ¡Entendido! Una crema efectiva para{' '}
-                    <span className="font-semibold">melasma</span> contiene:
-                  </Text>
-
                   <Flex className="justify-center w-full">
                     <Image
                       src="/images/derma/multistep/ingredients/acidoRetinoico.png"
@@ -57,24 +59,24 @@ export default function Feedback() {
                     />
                   </Flex>
 
-                  <ul className="flex flex-col gap-4 w-full mb-8">
-                    <li className="flex items-start gap-3 w-full text-sm">
+                  <ul className="flex flex-col gap-4 md:gap-6 w-full mb-8 md:mb-16 text-sm md:text-lg">
+                    <li className="flex items-start gap-3 w-full">
                       <SvgVerify className="text-derma-primary500 shrink-0" />
                       <div>
-                        <Text className="font-semibold mb-2">
+                        <Text className="font-semibold mb-1">
                           Ácido retinoico
                         </Text>
                         <Text>0,015% - 0,05% Despigmentante y regenerador</Text>
                       </div>
                     </li>
-                    <li className="flex items-start gap-3 w-full text-sm">
+                    <li className="flex items-start gap-3 w-full">
                       <SvgVerify className="text-derma-primary500 shrink-0" />
                       <div>
-                        <Text className="font-semibold mb-2">
+                        <Text className="font-semibold mb-1">
                           Hidroquinona *
                         </Text>
                         <Text>4% Despigmentante</Text>
-                        <Text className="text-xs text-hg-black500">
+                        <Text className="text-xs md:text-md text-hg-black500">
                           *En verano sustituimos la Hidroquinona por una
                           combinación de Ácido kójico (4%) y Ácido glicólico
                           (8%)
@@ -87,7 +89,7 @@ export default function Feedback() {
                     <div className="bg-derma-primary500 rounded-full p-2">
                       <SvgStethoscope className="text-white h-4 w-4" />
                     </div>
-                    <Text className="text-xs text-hg-black500">
+                    <Text className="text-xs md:text-sm text-hg-black500">
                       Un médico valorará tu caso y ajustará tu crema
                       personalizada
                     </Text>
@@ -98,11 +100,18 @@ export default function Feedback() {
                       type="white"
                       customStyles="bg-transparent border-none"
                       onClick={() => router.back()}
+                      size={isMobile ? 'md' : 'lg'}
                     >
                       <SvgArrow className="h-4 w-4 rotate-180 mr-2" />
                       <Text className="text-derma-tertiary">Atrás</Text>
                     </Button>
-                    <Button type="derma">continuar</Button>
+                    <Button
+                      type="derma"
+                      size={isMobile ? 'md' : 'lg'}
+                      href={ROUTES.derma.multistep.skinType}
+                    >
+                      continuar
+                    </Button>
                   </Flex>
                 </Flex>
               </Flex>
