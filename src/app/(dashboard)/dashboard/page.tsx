3521b96aef4282589279afadb32b203923010f95
Lluis <lluistallada@holaglow.com>
2024-05-31 17:06:10 +0200

    reset professionalsModal visiblity in dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -45,8 +45,12 @@ export default function Page({
   const messageSocket = useMessageSocket(state => state);
   const { setCurrentUser, setAppointmentId, setBudgetId } =
     useGlobalPersistedStore(state => state);
-  const { setSelectedTreatments, setSelectedClinic, setSelectedPack } =
-    useSessionStore(state => state);
+  const {
+    setSelectedTreatments,
+    setSelectedClinic,
+    setSelectedPack,
+    setHasSeenDashboardProfessionals,
+  } = useSessionStore(state => state);
   const {
     remoteControl,
     storedBoxId,
@@ -152,6 +156,7 @@ export default function Page({
     setExtraInfo(params.get('extraInfo') == 'true');
     const phone = params.get('phoneNumber') || '';
     setPhoneNumber(phone.length > 9 ? phone.slice(3, phone.length) : phone);
+    setHasSeenDashboardProfessionals(false);
   }, []);
 
   useEffect(() => {
