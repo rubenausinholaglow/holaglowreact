8c013c5171329a86d8b42aff4f50dc9b4e763741
Lluis <lluistallada@holaglow.com>
2024-06-18 14:22:20 +0200

    update tests and config

--- tests/Home.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.holaglow.com/');
-  await page
-    .getByRole('heading', { name: 'Medicina estética para cuidar' })
-    .click();
-});
