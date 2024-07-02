88e5f983b9336fe4c9a71ab0f6d740d67c72deac
Lluis <lluistallada@holaglow.com>
2024-07-02 10:03:51 +0200

    test envs in playwright

--- e2e/HomeIsRendered.spec.ts
+++ e2e/HomeIsRendered.spec.ts
@@ -1,6 +1,9 @@
 import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  if (process.env.BASE_URL) {
+    await page.goto(process.env.BASE_URL);
+  }
   await expect(
     page.getByRole('heading', { name: 'Medicina estética para cuidar' })
   ).toBeVisible();
